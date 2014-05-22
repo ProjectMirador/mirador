e14b2e08fdacc5ce176ae3276762fddced89f6eb
rsinghal <rashmi.s.singhal@gmail.com>
2014-05-22 13:22:15 -0400

    workspace events

--- js/src/workspace.js
+++ js/src/workspace.js
@@ -3,12 +3,13 @@
   $.Workspace = function(options) {
 
      jQuery.extend(true, this, {
-         type: null,
-         workspaceSlotCls: 'slot'
+         type:             null,
+         workspaceSlotCls: 'slot',
+         objects:          []
          
      }, $.DEFAULT_SETTINGS, options);
      
-     this.element  = this.element || jQuery('<div class="workspaceContainer">');
+     this.element  = this.element || jQuery('<div id="workspaceContainer">');
      
      this.init();
 
@@ -24,7 +25,34 @@
                 workspaceSlotCls: this.workspaceSlotCls
             }));
             jQuery(this.element).layout({ applyDefaultStyles: true });
+            
+            this.bindEvents();
       },
+      
+      bindEvents: function() {
+            var _this = this;
+            
+            jQuery.subscribe('manifestToWorkspace', function(_, manifest) {
+                _this.objects = [manifest];
+            });
+            
+            jQuery.subscribe('currentWorkspaceVisible.set', function(_, stateValue) {
+                if (stateValue) { _this.show(); return; }
+                _this.hide();
+            });
+      },
+      
+      hide: function() {
+            var _this = this;
+            _this.element.removeClass('active');
+      },
+
+      show: function() {
+            var _this = this;
+            _this.element.addClass('active');
+      },
+      
+      //template should be based on workspace type
       template: Handlebars.compile([
       '<div class="{{workspaceSlotCls}} ui-layout-center">',
           '<span>',
