8e335284b174980dabcb2e563f54263b18ee552f
rsinghal <rashmi.s.singhal@gmail.com>
2014-07-24 15:58:38 -0400

    add icons for annotations and image choices.  some css issues

--- build/mirador/mirador.js
+++ build/mirador/mirador.js
@@ -5240,6 +5240,7 @@ jQuery.fn.scrollStop = function(callback) {
         minZoomLevel:     0,
         defaultZoomLevel: 0,
         prefixUrl:        'images/openseadragon/',
+        autoHideControls: false,
         navigationControlAnchor:    OpenSeadragon.ControlAnchor.BOTTOM_RIGHT,
         navImages: {
           zoomIn: {
@@ -5294,6 +5295,20 @@ jQuery.fn.scrollStop = function(callback) {
 
     osd.addControl(div, {anchor: OpenSeadragon.ControlAnchor.BOTTOM_RIGHT});
     
+    div = document.createElement("div");
+    var anno = document.createElement("a");
+    anno.className = 'mirador-btn mirador-icon-annotations';
+    var i = document.createElement("i");
+    i.className = "fa fa-comments fa-lg";
+    anno.appendChild(i);
+    
+    var choices = document.createElement("a");
+    choices.className = "mirador-btn mirador-icon-choices";
+
+	div.appendChild(anno);
+	div.appendChild(choices);
+	osd.addControl(div, {anchor: OpenSeadragon.ControlAnchor.BOTTOM_LEFT});
+
     return osd;
 
   };
--- js/src/utils/openSeadragon.js
+++ js/src/utils/openSeadragon.js
@@ -11,6 +11,7 @@
         minZoomLevel:     0,
         defaultZoomLevel: 0,
         prefixUrl:        'images/openseadragon/',
+        autoHideControls: false,
         navigationControlAnchor:    OpenSeadragon.ControlAnchor.BOTTOM_RIGHT,
         navImages: {
           zoomIn: {
@@ -65,6 +66,20 @@
 
     osd.addControl(div, {anchor: OpenSeadragon.ControlAnchor.BOTTOM_RIGHT});
     
+    div = document.createElement("div");
+    var anno = document.createElement("a");
+    anno.className = 'mirador-btn mirador-icon-annotations';
+    var i = document.createElement("i");
+    i.className = "fa fa-comments fa-lg";
+    anno.appendChild(i);
+    
+    var choices = document.createElement("a");
+    choices.className = "mirador-btn mirador-icon-choices";
+
+	div.appendChild(anno);
+	div.appendChild(choices);
+	osd.addControl(div, {anchor: OpenSeadragon.ControlAnchor.BOTTOM_LEFT});
+
     return osd;
 
   };
