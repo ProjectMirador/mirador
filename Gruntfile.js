c28e53330411d1ea60025edc11e4b64d0d13b998
aeschylus <scipioaffricanus@gmail.com>
2014-12-10 04:25:14 -0800

    more config changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -206,15 +206,7 @@ module.exports = function(grunt) {
     },
     
     coveralls: {
-      options: {
-        // LCOV coverage file relevant to every target
-        src: 'reports/coverage/lcov.info',
-
-        // When true, grunt-coveralls will only print a warning rather than
-        // an error, to prevent CI builds from failing unnecessarily (e.g. if
-        // coveralls.io is down). Optional, defaults to false.
-        force: false
-      }
+      src: 'reports/coverage/lcov.info',
     },
 
     jasmine: {
