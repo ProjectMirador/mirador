f068fb83ee8584ea1ba5e1cf365cd2a7d45ba872
aeschylus <scipioaffricanus@gmail.com>
2015-01-15 23:23:58 -0800

    fixed jshint errors

--- js/src/workspace.js
+++ js/src/workspace.js
@@ -26,11 +26,6 @@
 
       this.calculateLayout();
 
-      if (this.focusedSlot === null) {
-        // set the focused slot to the first in the list
-        this.focusedSlot = this.slots[0].slotID;
-      }
-
       this.bindEvents();
     },
     slotList: function(layoutSlots) {
@@ -78,12 +73,21 @@
       .attr("class", "layout-slot")
       .attr("data-layout-slot-id", function(d) { return d.id; })
       .call(cell)
-      .each(function(d) { console.log(d); });
+      .each(function(d) {
+        var appendTo = _this.element.children('div').filter('[data-layout-slot-id="'+ d.id+'"]')[0];
+        _this.slots.push(new $.Slot({
+          slotID: d.id,
+          focused: true,
+          parent: _this,
+          appendTo: appendTo
+        }));
+      });
 
       // Exit
       divs.exit()
       .remove("div")
       .each(function(d) { 
+        console.log(_this.slots);
         var slotMap = _this.slots.reduce(function(map, temp_slot) {
             map[d.id] = temp_slot;
             return map;
@@ -93,6 +97,7 @@
           jQuery.publish("windowRemoved", slot.window.id);
         }
         _this.slots.splice(_this.slots.indexOf(slot), 1);
+        console.log(_this.slots);
       });
 
       function cell() {
@@ -103,58 +108,162 @@
         .style("height", function(d) { return Math.max(0, d.dy ) + "px"; });
       }
 
-      _this.slotList(data);
     },
 
-    splitLeft: function() {
+    split: function(targetSlot, direction) {
+      var _this = this,
+      node = jQuery.grep(_this.layout, function(node) { return node.id === targetSlot.slotID; })[0];
+      console.log(node);
+      nodeIndex = node.parent ? node.parent.children.indexOf(node) : node;
+
+      function addSibling(node, beforeOrAfter) {
+        newSibling = _this.newNode(node.type, node.address.concat("." + node.type + '1'), node);
+        node.parent.children.splice(nodeIndex + 1, 0, newSibling);
+        _this.layout.push(newSibling);
+      }
+
+      function mutateAndAdd(node, beforeOrAfter) {
+        newParent = _this.newNode(node.type, node.address, node.parent),
+        oldAddress = node.address;
+
+        // Recalculate the address of this node
+        // and flip its type while keeping
+        // the same id.
+        node.type = node.type === 'row' ? 'column' : 'row';
+        node.address = node.address.concat(node.type + '1');
+
+        // Create a new node (which will be childless)
+        // that is also a sibling of this node.
+        newSibling = _this.newNode(node.type, oldAddress.concat("." + node.type + '1'), newParent);
+
+        // maintain array ordering.
+        newParent.children = [];
+        newParent.children.push(node, newSibling); // order matters.
+        newParent.parent = node.parent;
+
+        // replace the old node in ites parent's child
+        // array with the new parent.
+        newParent.parent.children[nodeIndex] = newParent;
+
+        node.parent = newParent;
+        _this.layout.push(newParent, newSibling);
+      }
+
+      if (node.type === 'column') {
+        // Since it is a column:
+        // 
+        // If adding to a side, simply
+        // add a sibling.
+        // Left means before, right means after.
+        if (direction === 'r' || direction === 'l') {
+          addSibling(node, direction);
+        } 
+        // If adding above or below, the
+        // operation must be changed to mutating
+        // the structure. 
+        // Up means before, Down means after.
+        else {
+          mutateAndAdd(node, direction);
+        }
+      } else {
+        // Since it is a row:
+        //
+        // If adding to a side, mutate the 
+        // structure.
+        // Left means before, right means after.
+        if (direction === 'r' || direction === 'l') {
+          mutateAndAdd(node, direction);
+        } 
+        // If adding above or below, the
+        // operations must be switched to adding
+        // a sibling. 
+        // Up means before, Down means after.
+        else {
+          addSibling(node, direction);
+        }
+      }
+      
+      // Recalculate the layout.
+      // The original hierarchical structure is
+      // accessible from the root node. Passing 
+      // it back through the layout code will 
+      // recalculate everything else needed for 
+      // the redraw.
+      var root = jQuery.grep(_this.layout, function(node) { return !node.parent;})[0];
+      _this.parent.workspaces[_this.parent.currentWorkspaceType].layout = root;
+      _this.calculateLayout();
 
     },
 
-    splitRight: function() {
-
+    splitRight: function(targetSlot) {
+      var _this = this;
+      _this.split(targetSlot, 'r');
     },
 
     splitDown: function(targetSlot) {
       var _this = this,
-      node = jQuery.grep(_this.layout, function(node) { return node.id === targetSlot.slotID; })[0],
+      node = jQuery.grep(_this.layout, function(node) { return node.id === targetSlot.slotID; })[0];
+      console.log(node);
+      nodeIndex = node.parent ? node.parent.children.indexOf(node) : node;
 
-      // if it's a column, 
-      // create a new node that is the parent
-      // of this node.
-      newParent = _this.newNode(node.type, node.address, node.parent),
-      oldAddress = node.address;
+      if (node.type === 'column') {
+        // if it's a column, 
+        // create a new node that is the parent
+        // of this node.
+        newParent = _this.newNode(node.type, node.address, node.parent),
+        oldAddress = node.address;
 
-      // Recalculate the address of this node
-      // and flip its type while keeping
-      // the same id.
-      node.type = node.type === 'row' ? 'column' : 'row';
+        // Recalculate the address of this node
+        // and flip its type while keeping
+        // the same id.
+        node.type = node.type === 'row' ? 'column' : 'row';
 
-      node.address = node.address.concat(node.type + '1');
+        node.address = node.address.concat(node.type + '1');
 
-      // Create a new node (which will be childless)
-      // that is also a sibling of this node.
-      newSibling = _this.newNode(node.type, oldAddress.concat("." + node.type + '1'), newParent);
+        // Create a new node (which will be childless)
+        // that is also a sibling of this node.
+        newSibling = _this.newNode(node.type, oldAddress.concat("." + node.type + '1'), newParent);
+
+        // maintain array ordering.
+        newParent.children = [];
+        newParent.children.push(node, newSibling);
+        newParent.parent = node.parent;
+
+        // replace the old node in ites parent's child
+        // array with the new parent.
+        newParent.parent.children[nodeIndex] = newParent;
+
+        node.parent = newParent;
+        _this.layout.push(newParent);
+      } else {
+        // if it is a row, simply create a 
+        // new sibling and insert it into the 
+        // correct location (taking into account
+        // the other siblings).
+        newSibling = _this.newNode(node.type, node.address.concat("." + node.type + '1'), node);
+        node.parent.children.splice(nodeIndex + 1, 0, newSibling);
+        _this.layout.push(newSibling);
+      }
       
-      // maintain array ordering.
-      newParent.children = [];
-      newParent.children.push(node, newSibling);
-      newParent.parent = node.parent;
-
-      // replace the old node in ites parent's child
-      // array with the new parent.
-      var nodeIndex = newParent.parent.children.indexOf(node);
-      newParent.parent.children[nodeIndex] = newParent;
-
-      // Recalculate the layout (just UI code????).
-      node.parent = newParent;
-      _this.layout.push(newParent, newSibling);
+      // Recalculate the layout.
+      // The original hierarchical structure is
+      // accessible from the root node. Passing 
+      // it back through the layout code will 
+      // recalculate everything else needed for 
+      // the redraw.
       var root = jQuery.grep(_this.layout, function(node) { return !node.parent;})[0];
       _this.parent.workspaces[_this.parent.currentWorkspaceType].layout = root;
       _this.calculateLayout();
     },
 
-    splitUp: function() {
+    splitUp: function(targetSlot) {
+      var _this = this;
+      _this.split(targetSlot, 'u');
+    },
 
+    splitDwn: function(targetSlot) {
+      var _this = this;
+      _this.split(targetSlot, 'd');
     },
 
     newNode: function(type, address, parent) {
