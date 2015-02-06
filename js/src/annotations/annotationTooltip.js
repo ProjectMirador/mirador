e85a606ca1af4f703dfd19d4e091df04fd954920
rsinghal <rashmi.s.singhal@gmail.com>
2015-02-06 14:42:02 -0500

    qtip editor and stack annotations for overlapping rectangles.  still buggy

---
+++ js/src/annotations/annotationTooltip.js
@@ -0,0 +1,90 @@
+(function($) {
+
+  $.AnnotationTooltip = function(options) {
+
+    jQuery.extend(this, {
+      element:   null,
+      parent:    null,
+      annotations: []
+    }, options);
+
+    this.init();
+  };
+
+  $.AnnotationTooltip.prototype = {
+
+    init: function() {
+      this.bindEvents();
+    },
+
+    bindEvents: function() {
+      var _this = this;
+    },
+    
+    getViewer: function(annotations) {
+      var annoText,
+        tags = [],
+        _this = this,
+        htmlAnnotations = [];
+
+        jQuery.each(annotations, function(index, annotation) {
+        if (jQuery.isArray(annotation.resource)) {
+          jQuery.each(annotation.resource, function(index, value) {
+            if (value['@type'] === "dctypes:Text") {
+              annoText = value.chars;
+            } else if (value['@type'] == "oa:Tag") {
+              tags.push(value.chars);
+            }
+          });
+        } else {
+          annoText = annotation.resource.chars;
+        }
+        htmlAnnotations.push({
+           annoText: annoText,
+           tags: tags
+           });
+        });
+
+        return this.viewerTemplate({annotations : htmlAnnotations});
+        //return combination of all of them
+    },
+    
+    //when this is being used to edit an existing annotation, insert them into the inputs
+    editorTemplate: Handlebars.compile([
+     '<form class="new-annotation-form">',
+         '<ul>',
+          '<li>',
+           '<textarea class="text-editor" placeholder="Comments…"></textarea>',
+          '</li>',
+          '<li>',
+           '<input class="tags-editor" placeholder="Add some tags here…">',
+          '</li>',
+         '</ul>',
+         '<div>',
+         //need to add a delete, if permissions allow
+          '<a href="#cancel" class="cancel">Cancel</a>',
+          '<a href="#save" class="save">Save</a>',
+         '</div>',
+        '</form>'
+    ].join('')),
+    
+    viewerTemplate: Handlebars.compile([
+      '<div class="all-annotations">',
+      '{{#each annotations}}',
+      '<div class="annotation-display">',
+       '<a href="#edit" class="edit">Edit</a>',
+       '<a href="#delete" class="delete">Delete</a>',
+       '<div class="text-viewer">{{annoText}}</div>',
+       '<div class="tags-viewer">',
+       '{{#each tags}}',
+       '<span class="tag">{{this}}</span>',
+       '{{/each}}',
+       '</div>',
+       '</div>',
+       '{{/each}}',
+      '</div>'                      
+    ].join(''))
+
+  };
+
+}(Mirador));
--- js/src/annotations/osd-canvas-renderer.js
+++ js/src/annotations/osd-canvas-renderer.js
@@ -12,7 +12,9 @@
      osdViewer: null,
      elements:  null,
      list:      null,
-     parent:    null
+     parent:    null,
+     annoTooltips: {},
+     tooltips:  null
     }, options);
   };
   
@@ -63,8 +65,15 @@
         } else {
           annoText = annotation.resource.chars;
         }
-        jQuery(osdOverlay).qtip({
-           overwrite : false,
+        /*var annoOverlay = document.createElement('div');
+        annoOverlay.className = 'osd-annotation-overlay';
+        _this.osdViewer.addOverlay({
+          element: annoOverlay,
+          location: _this.getOsdFrame(region)
+        });*/
+        /*var tooltip = jQuery(osdOverlay).qtip({
+           //overwrite : false,
+           prerender: true,
            content: {
             title : annoText,
             text : tags
@@ -81,11 +90,26 @@
             },
             hide: {
                 fixed: true,
-                delay: 300
+                delay: 100
+            },
+            show: {
+              event: false
+            },
+            events : {
+              visible: function(event, api) {
+                //console.log("showing this");
+                //console.log(api);
+                //console.log(jQuery('.qtip.qtip-bootstrap'));
+              },
+              focus: function(event, api){
+                //console.log(jQuery('.qtip.qtip-bootstrap[aria-hidden="false"]').height());
+                
+              }
             }
-         });
+         });*/
+         //_this.annoTooltips[annotation['@id']] = tooltip;
       });
-
+      //console.log(_this.annoTooltips);
       this.bindEvents();
     },
     
@@ -125,17 +149,67 @@
       //console.log(overlays);
     },
     
+    getAnnoIdsFromPosition: function(event, imageViewElem) {
+      var _this = this,
+      annos = imageViewElem.find('.annotation').map(function() {
+        var self = jQuery(this),
+        offset = self.offset(),
+        l = offset.left,
+        t = offset.top,
+        h = self.height(),
+        w = self.width(),
+        x = new OpenSeadragon.getMousePosition(event).x,
+        y = new OpenSeadragon.getMousePosition(event).y,
+        maxx = l+w,
+        maxy = t+h;
+        
+        return (y <= maxy && y >= t) && (x <= maxx && x >= l) ? this.id : null;
+      });
+      
+      return annos;
+    },
+    
+    getOverlaysFromPosition: function(event, imageViewElem) {
+      var _this = this,
+      annos = imageViewElem.find('.annotation').map(function() {
+        var self = jQuery(this),
+        offset = self.offset(),
+        l = offset.left,
+        t = offset.top,
+        h = self.height(),
+        w = self.width(),
+        x = new OpenSeadragon.getMousePosition(event).x,
+        y = new OpenSeadragon.getMousePosition(event).y,
+        maxx = l+w,
+        maxy = t+h;
+        
+        return (y <= maxy && y >= t) && (x <= maxx && x >= l) ? this : null;
+      });
+      
+      return annos;
+    },
+    
     bindEvents: function() {
       var _this = this;
       // be sure to properly delegate your event handlers
       jQuery(this.osdViewer.canvas).parent().on('click', '.annotation', function() { _this.onSelect(); });
 
-      /*jQuery(this.osdViewer.canvas).parent().parent().on('mouseenter', '.annotation', function() { 
-        _this.onHover(_this.getAnnoFromRegion(jQuery(this)[0].id)); 
+      /*jQuery(this.osdViewer.canvas).parent().parent().on('mouseover', '.annotation', function() { 
+        //_this.onHover(_this.getAnnoFromRegion(jQuery(this)[0].id)); 
+        console.log("entered an annotation overlay");
+        console.log(jQuery(this));
+        _this.onHover(event, jQuery(this)); 
+        console.log(_this.osd);
+        console.log(_this.osdViewer);
+      });*/
+                  
+      /*jQuery('.annotation').mouseenter( function(event) { 
+        console.log("entered an annotation overlay");
       });*/
       
       jQuery(this.osdViewer.canvas).parent().parent().on('mousemove', function(event) { 
-        _this.onHover(event, _this.getAnnotationsFromPosition(event, jQuery(this))); 
+        _this.onHover(event, _this.getOverlaysFromPosition(event, jQuery(this)));
+        //_this.onHover(event, _this.getAnnotationsFromPosition(event, jQuery(this))); 
       });
       
       /*jQuery(this.osdViewer.canvas).parent().on('mouseleave', '.annotation', function() {
@@ -159,25 +233,106 @@
       return elements;
     },
     
-    onHover: function(event, annotations) {
+    onHover: function(event, overlays) {
       //first hide all annotations and then find new ones to display
-      this.parent.annotator.viewer.hide();
-    
+      //this.parent.annotator.viewer.hide();
+      /*jQuery.each(this.annoTooltips, function(key, value) {
+        var api = value.qtip('api');
+        api.hide();
+      });*/
       var renderAnnotations = [],
-      _this = this;
-      jQuery.each(annotations, function(index, annotation) {
-        renderAnnotations = renderAnnotations.concat(_this.parent.prepareForAnnotator(annotation));
+      _this = this,
+      annoTooltip = new $.AnnotationTooltip(); //pass permissions
+      var offset = 0,
+      annotations = [];
+      //console.log(overlays);
+      jQuery.each(overlays, function(index, overlay) {
+        //var annotation = _this.getAnnoFromRegion(id)[0];
+        //console.log(annotation);
+        //console.log(overlay);
+        //console.log(id);
+        //var api = _this.annoTooltips[id].qtip('api');
+        //console.log(api.get('position.adjust'));
+        //console.log(api.elements.tooltip.height());
+        //console.log(offset);
+        //api.set('position.adjust.y', offset);
+        //console.log(api.get('position.adjust'));
+        //api.show();
+        //api.reposition();
+        //offset = offset + jQuery(api.elements.tooltip).height();
+        annotations.push(_this.getAnnoFromRegion(overlay.id)[0]);
+
       });
-      //var annotation = annotations[0];
-      //var position = this.parent.parseRegionForAnnotator(annotation.on);
+      /*var annoText,
+        tags = [];
+        if (jQuery.isArray(annotation.resource)) {
+          jQuery.each(annotation.resource, function(index, value) {
+            if (value['@type'] === "dctypes:Text") {
+              annoText = value.chars;
+            } else if (value['@type'] == "oa:Tag") {
+              tags.push(value.chars);
+            }
+          });
+        } else {
+          annoText = annotation.resource.chars;
+        }*/
+        if (annotations.length > 0) {
+          //console.log(overlays.first());
+          if (this.tooltips) {
+            var api = this.tooltips.qtip('api');
+            api.set({'content.text' : annoTooltip.getViewer(annotations),
+            'show.target' : overlays.first(),
+            'position.target' : 'mouse'});
+            api.reposition();
+          } else {
+           this.tooltips = jQuery(_this.osdViewer.element).qtip({
+            overwrite : false,
+            content: {
+             text : annoTooltip.getViewer(annotations)
+             },
+             position : {
+              target : 'mouse',
+              //viewport : jQuery(_this.osdViewer.element),
+              adjust : {
+                mouse: true
+              }
+             },
+             style : {
+              classes : 'qtip-bootstrap'
+             },
+             show: {
+              target: overlays.first(),
+              ready: true,
+              delay: 90
+             },
+             hide: {
+                fixed: true,
+                delay: 0
+             },
+             events: {
+               hide: function(event, api) {
+                 //api.destroy();
+               },
+               show: function(event, api) {
+                 //api.reposition();
+               }
+             } 
+            });
+           }
+        } else {
+          if (this.tooltips) {
+            //this.tooltips.qtip('api').hide();
+          }
+        }
+      
       
       //need to account for various menu bars and side panel that affect the mouse position
-      var topOffset = jQuery(window).height() - _this.osdViewer.container.offsetHeight-2; //subtract a few pixels so mouse pointer is closer to annotation
-      var leftOffset = jQuery(window).width()-_this.osdViewer.container.offsetWidth;
-      var position = {
-                    top: new OpenSeadragon.getMousePosition(event).y-topOffset,
-                    left: new OpenSeadragon.getMousePosition(event).x-leftOffset
-                };
+      //var topOffset = jQuery(window).height() - _this.osdViewer.container.offsetHeight-2; //subtract a few pixels so mouse pointer is closer to annotation
+      //var leftOffset = jQuery(window).width()-_this.osdViewer.container.offsetWidth;
+      //var position = {
+      //              top: new OpenSeadragon.getMousePosition(event).y-topOffset,
+      //              left: new OpenSeadragon.getMousePosition(event).x-leftOffset
+      //          };
       //this.parent.annotator.showViewer(this.parent.prepareForAnnotator(annotation), position);
       //if (renderAnnotations.length > 0) {this.parent.annotator.showViewer(renderAnnotations, position);}
     },
