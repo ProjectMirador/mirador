a0e9f4241cf29ddc2214f71025deddc851b068d2
Fabio Torchetti <fabio.torchetti@wedjaa.net>
2015-05-14 17:25:48 -0500

    Rebased after endpoint fix

--- js/src/workspaces/window.js
+++ js/src/workspaces/window.js
@@ -594,19 +594,21 @@
         var dfd = jQuery.Deferred(),
         module = $.viewer.annotationEndpoint.module,
         options = $.viewer.annotationEndpoint.options;
-        if (_this.endpoint && _this.endpoint !== null) {
-          endpoint.set('dfd', dfd);
-          endpoint.search(_this.currentCanvasID);
-          // update with new search
+        // One annotation endpoint per window, the endpoint
+        // is a property of the instance.
+        if ( _this.endpoint && _this.endpoint != null ) {
+          _this.endpoint.set('dfd', dfd);
+          _this.endpoint.search(_this.currentCanvasID);
         } else {
           options.element = _this.element;
           options.uri = _this.currentCanvasID;
           options.dfd = dfd;
           options.windowID = _this.id;
-          endpoint = new $[module](options);
+          _this.endpoint = new $[module](options);
         }
+
         dfd.done(function(loaded) {
-          _this.annotationsList = _this.annotationsList.concat(endpoint.annotationsList);
+          _this.annotationsList = _this.annotationsList.concat(_this.endpoint.annotationsList);
           // clear out some bad data
           _this.annotationsList = jQuery.grep(_this.annotationsList, function (value, index) {
             if (typeof value.on === "undefined") { 
