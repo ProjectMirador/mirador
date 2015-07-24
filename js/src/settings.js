7c409d58d7a21a95b65940c5fed976afe6196b5f
rsinghal <rashmi.s.singhal@gmail.com>
2015-07-24 10:51:12 -0400

    allow for different i18n path from settings

--- js/src/settings.js
+++ js/src/settings.js
@@ -95,6 +95,8 @@
     //true or false.  controls display of "Add new object from URL" on manifest listing page
     'showAddFromURLBox' : true,
 
+    'i18nPath' : 'build/mirador/locales/',
+
     'logosLocation' : 'build/mirador/images/logos/',
 
     'repoImages' : {
