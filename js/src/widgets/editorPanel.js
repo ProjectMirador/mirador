16d8b50486da96a05df86618a055f2110148c619
Shaun Ellis <shaun@sdellis.com>
2015-08-06 17:42:01 -0400

    paints annotations to the editorPanel

--- js/src/widgets/editorPanel.js
+++ js/src/widgets/editorPanel.js
@@ -44,25 +44,41 @@
 
             return this.panelState;
         },
-        openAnnotationList: function() {
-            var state = this.state(),
+        openAnnotationList: function(listId) {
+            var _this = this,
+                state = this.state(),
                 open = !state.open;
+            var window = $.viewer.workspace.windows.map(function(window){
+                  if(window.id == _this.windowId){ return window; }
+                });
+
+            state.annotations = window[0].annotationsList.filter(function(annotation){
+              if(annotation.endpoint === listId){
+                return true;
+              }
+
+              if(annotation.endpoint.name === listId){
+                return true;
+              }
+
+              return false;
+            });
 
             state.open = open;
             this.state(state);
         },
-        getTemplateData: function() {
+        getTemplateData: function(state) {
             return {
-                postion: this.state().position,
-                size:  this.state().size
+                annotations: state.annotations,
+                postion: state.position,
+                open: state.open,
+                size:  state.size
             };
         },
         listenForActions: function() {
             var _this = this;
 
             jQuery.subscribe('editorPanelStateUpdated' + this.windowId, function(_, data) {
-                console.log('editorPanelToggled now');
-                console.log(data);
                 _this.render(data);
             });
 
@@ -80,19 +96,31 @@
         bindEvents: function() {
             var _this = this;
         },
-        render: function(renderingData) {
+        render: function(state) {
             var _this = this;
+            var templateData = _this.getTemplateData(state);
 
             if (!this.element) {
-                this.element = jQuery(_this.template(renderingData)).appendTo(_this.appendTo);
+                this.element = jQuery(_this.template(templateData)).appendTo(_this.appendTo);
                 return;
-            }
+            } else {
+                _this.appendTo.find(".editorPanel").empty();
+                var contents = jQuery(_this.template(templateData)).children();
 
-            var openValue = renderingData.open === true ? 'block' : 'none';
+                this.element.html(contents);
+            }
+            var openValue = templateData.open === true ? 'block' : 'none';
             this.element.css({'display':openValue});
         },
         template: Handlebars.compile([
             '<div class="editorPanel {{position}}">',
+            '<ul class="annotations">',
+            '{{#each annotations}}',
+            '<li class="annotationItem">',
+                '<span>{{this.resource.chars}}{{#each this.resource}}{{chars}}{{/each}}</span>',
+            '</li>',
+            '{{/each}}',
+            '</ul>',
             '</div>'
         ].join('')),
         toggle: function () {}
