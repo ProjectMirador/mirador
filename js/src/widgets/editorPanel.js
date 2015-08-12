e32fd30361ee0d3ecb5767705db583fb84c8e138
Shaun Ellis <shaun@sdellis.com>
2015-08-12 16:50:29 -0400

    editor toggle now works

--- js/src/widgets/editorPanel.js
+++ js/src/widgets/editorPanel.js
@@ -44,10 +44,15 @@
 
             return this.panelState;
         },
-        openAnnotationList: function(listId) {
+        openAnnotationList: function(selected) {
             var _this = this,
                 state = this.state(),
                 open = !state.open;
+
+            var listId = selected.listId;
+            var prev = selected.prev;
+            if (listId !== prev && prev !== null){ open = true; }
+
             var window = $.viewer.workspace.windows.map(function(window){
                   if(window.id == _this.windowId){ return window; }
                 });
@@ -92,6 +97,7 @@
             jQuery.subscribe('openAnnotationList.' + this.windowId, function(event, data) {
                 _this.openAnnotationList(data);
             });
+
         },
         bindEvents: function() {
             var _this = this;
