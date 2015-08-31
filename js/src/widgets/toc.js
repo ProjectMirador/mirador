b75e59761dbe595fb74e7ad5fa77db1daa65f867
rsinghal <rashmi.s.singhal@gmail.com>
2015-08-31 15:43:21 -0400

    remove label from toc span

--- js/src/widgets/toc.js
+++ js/src/widgets/toc.js
@@ -307,7 +307,7 @@
       Handlebars.registerHelper('tocLevel', function(id, label, level, children) {
         var caret = '<i class="fa fa-caret-right caret"></i>',
         cert = '<i class="fa fa-certificate star"></i>';
-        return '<h' + (level+1) + '><a class="toc-link" data-rangeID="' + id + '">' + caret + cert + '<span class="label">' + label + '</span></a></h' + (level+1) + '>';
+        return '<h' + (level+1) + '><a class="toc-link" data-rangeID="' + id + '">' + caret + cert + '<span>' + label + '</span></a></h' + (level+1) + '>';
       });
 
       return template(tplData);
