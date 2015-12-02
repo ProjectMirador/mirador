06c9413469dd42eea326291b954105ae2113626d
aeschylus <scipioaffricanus@gmail.com>
2015-12-02 11:56:16 -0800

    added modified simpleASEndpoint that will work by default with Simple Annotation Endpoint Server

--- js/src/annotations/simpleASEndpoint.js
+++ js/src/annotations/simpleASEndpoint.js
@@ -1,25 +1,25 @@
 /*
-* Edited version of https://github.com/IIIF/mirador/blob/9e3c6bbb894e044d01ad51aae1b70309939de5a9/js/src/annotations/catchEndpoint.js 
-* This module tries to store the annotation as is in a RDF store but some fiddeling is required. Fidles are:
-* 
-* - delete annotation fails if id has a / in it so have to send sanatised ids to mirador
-* - mirador requires an endpoint variable in the annotation pointing to this class.
-* 
-* Note: this endpoint doesn't currently support authentication, just returns allow all
-* 
-* All Endpoints need to have at least the following:
-* annotationsList - current list of OA Annotations
-* dfd - Deferred Object
-* init()
-* search(uri)
-* create(oaAnnotation, returnSuccess, returnError)
-* update(oaAnnotation, returnSuccess, returnError)
-* deleteAnnotation(annotationID, returnSuccess, returnError) (delete is a reserved word)
-* TODO:
-* There is a bug in that if you create an annotation and then delete it (without moving pages) then click either the write annotation button 
-* or try to create a new annotation the deleted annotation re-appears. Changing pages fixes the issue as the annoation is delete from the annotation store
-*
-*/
+ * Edited version of https://github.com/IIIF/mirador/blob/9e3c6bbb894e044d01ad51aae1b70309939de5a9/js/src/annotations/catchEndpoint.js 
+ * This module tries to store the annotation as is in a RDF store but some fiddeling is required. Fidles are:
+ * 
+ * - delete annotation fails if id has a / in it so have to send sanatised ids to mirador
+ * - mirador requires an endpoint variable in the annotation pointing to this class.
+ * 
+ * Note: this endpoint doesn't currently support authentication, just returns allow all
+ * 
+ * All Endpoints need to have at least the following:
+ * annotationsList - current list of OA Annotations
+ * dfd - Deferred Object
+ * init()
+ * search(uri)
+ * create(oaAnnotation, returnSuccess, returnError)
+ * update(oaAnnotation, returnSuccess, returnError)
+ * deleteAnnotation(annotationID, returnSuccess, returnError) (delete is a reserved word)
+ * TODO:
+ * There is a bug in that if you create an annotation and then delete it (without moving pages) then click either the write annotation button 
+ * or try to create a new annotation the deleted annotation re-appears. Changing pages fixes the issue as the annoation is delete from the annotation store
+ *
+ */
 (function($){
 
   $.SimpleASEndpoint = function(options) {
@@ -27,7 +27,6 @@
     jQuery.extend(this, {
       token:     null,
       // prefix:    'annotation', /**/
-      uri:       null,
       url:		  options.url,
       dfd:       null,
       annotationsList: [],        //OA list for Mirador use
@@ -52,50 +51,58 @@
           'admin':  [this.userid]
         }
       };
-      this.search(this.uri);
+      this.search(this.url);
     },
 
     //Search endpoint for all annotations with a given URI
-    search: function(uri) {
+    search: function(options, successCallback, errorCallback) {
       var _this = this;
 
       this.annotationsList = []; //clear out current list
       jQuery.ajax({
         url: _this.url + "/search", // this.prefix+
-          type: 'GET',
+        type: 'GET',
         dataType: 'json',
         headers: {
           //"x-annotator-auth-token": this.token
         },
         data: {
-          uri: uri,
+          uri: options.uri,
           media: "image",
           limit: 10000
         },
 
         contentType: "application/json; charset=utf-8",
         success: function(data) {
-          _this.annotationsList = data; // gmr
-          jQuery.each(_this.annotationsList, function(index, value) {
-            value.fullId = value["@id"];
-            value["@id"] = $.genUUID();
-            _this.idMapper[value["@id"]] = value.fullId;
-            value.endpoint = _this;
-          });
-          _this.dfd.resolve(false);
+          if (typeof successCallback === "function") {
+            successCallback(data);
+          } else {
+            _this.annotationsList = data; // gmr
+            jQuery.each(_this.annotationsList, function(index, value) {
+              value.fullId = value["@id"];
+              value["@id"] = $.genUUID();
+              _this.idMapper[value["@id"]] = value.fullId;
+              value.endpoint = _this;
+            });
+            _this.dfd.resolve(false);
+          }
         },
         error: function() {
-          console.log("error searching");
+          if (typeof errorCallback === "function") {
+            errorCallback();
+          } else {
+            console.log("The request for annotations has caused an error for endpoint: "+ options.uri);
+          }
         }
 
       });
     },
 
-    deleteAnnotation: function(annotationID, returnSuccess, returnError) {          
+    deleteAnnotation: function(annotationID, returnSuccess, returnError) {
       var _this = this;
       jQuery.ajax({
         url: _this.url + "/destroy?uri=" + _this.idMapper[annotationID], // this.prefix+
-          type: 'DELETE',
+        type: 'DELETE',
         dataType: 'json',
         headers: {
           //"x-annotator-auth-token": this.token
@@ -116,7 +123,7 @@
 
     update: function(oaAnnotation, returnSuccess, returnError) {
       var annotation = oaAnnotation,
-      _this = this;
+          _this = this;
       // slashes don't work in JQuery.find which is used for delete
       // so need to switch http:// id to full id and back again for delete.
       shortId = annotation["@id"];
@@ -126,7 +133,7 @@
       delete annotation.endpoint;
       jQuery.ajax({
         url: _this.url + "/update/"+annotationID, //this.prefix+
-          type: 'POST',
+        type: 'POST',
         dataType: 'json',
         headers: {
           //"x-annotator-auth-token": this.token
@@ -149,11 +156,11 @@
 
     create: function(oaAnnotation, returnSuccess, returnError) {
       var annotation = oaAnnotation,
-      _this = this;
+          _this = this;
 
       jQuery.ajax({
         url: _this.url + "/create", //this.prefix+
-          type: 'POST',
+        type: 'POST',
         dataType: 'json',
         headers: {
           //"x-annotator-auth-token": this.token
