523fa191be47e185fe4412126fde98ec1f960253
Mark Matney <mmatney@library.ucla.edu>
2016-05-04 15:26:50 -0400

    add 'widgets' stub test files

---
+++ spec/widgets/contextControls.test.js
@@ -0,0 +1,28 @@
+describe('ContextControls', function() {
+
+  beforeEach(function() {
+
+  });
+
+  afterEach(function() {
+
+  });
+
+  xdescribe('Initialization', function() {
+    it('should initialize', function() {
+
+    });
+  });
+
+  xdescribe('show', function() {
+
+  });
+
+  xdescribe('hide', function() {
+
+  });
+
+  xdescribe('bindEvents', function() {
+
+  });
+}); 
