9d59a4e902f1ea9b99230ae94d6dd78da541e4b3
Seong-June Kim <seong-june.kim@yale.edu>
2016-05-04 14:35:14 -0400

    Created pending tests that should be implemented later.

---
+++ spec/utils/iiif.test.js
@@ -0,0 +1,10 @@
+describe('iiif', function () {
+  xit('getImageUrl', function () {
+  });
+  xit('getVersionFromContext', function () {
+  });
+  xit('makeUriWithWidth', function () {
+  });
+  xit('getImageHostUrl', function () {
+  });
+});
---
+++ spec/workspaces/slot.test.js
@@ -0,0 +1,14 @@
+describe('workspaces/slot', function () {
+  xit('listenForActions', function () {
+  });
+  xit('bindEvents', function () {
+  });
+  xit('dropItem', function () {
+  });
+  xit('clearSlot', function () {
+  });
+  xit('getAddress', function () {
+  });
+  xit('addItem', function () {
+  });
+});
