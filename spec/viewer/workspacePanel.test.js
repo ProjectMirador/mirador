0fe3b1bf25e700357791cb8922736efdea74af0b
Arthur Barrett <abarrett@fas.harvard.edu>
2016-05-05 17:00:53 -0400

    Added tests for viewer/workspacePanel to bring up to 100% coverage.

--- spec/viewer/workspacePanel.test.js
+++ spec/viewer/workspacePanel.test.js
@@ -23,31 +23,62 @@ describe('WorkspacePanel', function() {
     expect(this.viewerDiv.find('.select-grid .grid-item').length).toBe(gridSize);
   });
   
-  it('can detect when a layout is selected', function() {
+  it('should listen for actions', function() {
+    spyOn(this.panel, 'onPanelVisible');
+    jQuery.publish('workspacePanelVisible');
+    expect(this.panel.onPanelVisible).toHaveBeenCalled();
+  });
+  
+  it('can detect layout selected', function() {
     var gridItem = this.viewerDiv.find('.grid-item').first();
     var gridString = gridItem.data('gridstring');
-    spyOn(this.panel, 'onSelect');
+    spyOn(this.panel, 'select');
     gridItem.trigger('click');
-    expect(this.panel.onSelect).toHaveBeenCalledWith(gridString);
+    expect(this.panel.select).toHaveBeenCalledWith(gridString);
   });
   
-  it('can detect when a layout is hovered', function() {
+  it('can detect layout hover', function() {
     var gridItem = this.viewerDiv.find('.grid-item').first();
     var gridString = gridItem.data('gridstring');
-    spyOn(this.panel, 'onHover');
+    spyOn(this.panel, 'hover');
     gridItem.trigger('mouseenter');
-    expect(this.panel.onHover).toHaveBeenCalledWith(gridString);
+    expect(this.panel.hover).toHaveBeenCalledWith(gridString);
+  });
+
+  it('can detect layout mouseout', function() {
+    var selectGrid = this.viewerDiv.find('.select-grid');
+    spyOn(this.panel, 'reset');
+    selectGrid.trigger('mouseout');
+    expect(this.panel.reset).toHaveBeenCalledWith();
   });
   
-  it('publishes events when a layout is selected', function() {
+  it('can reset itself', function() {
+    var el = this.panel.element;
+    this.panel.reset();
+    expect(el.find('.grid-text').css('display')).toBe('none');
+    expect(el.find('.grid-instructions').css('display')).not.toBe('none');
+    expect(el.find('.grid-item.hovered').length).toBe(0);
+  });
+
+  it('should display the grid selection when a layout is hovered', function() {
+    var rows = 2, cols = 2;
+    var gridString = rows+'x'+cols;
+    var el = this.panel.element;
+    this.panel.hover(gridString);
+    expect(el.find('.grid-instructions').css('display')).toBe('none');
+    expect(el.find('.grid-text').text()).toContain(gridString);
+    expect(el.find('.grid-item.hovered').length).toBe(rows*cols);
+  });
+
+  it('should publish events when a layout is selected', function() {
     var gridString = '2x2';
     var layoutDescription = Mirador.layoutDescriptionFromGridString(gridString);
     spyOn(jQuery, 'publish');
-    this.panel.onSelect(gridString);
+    this.panel.select(gridString);
     expect(jQuery.publish).toHaveBeenCalledWith('RESET_WORKSPACE_LAYOUT', {layoutDescription: layoutDescription});
     expect(jQuery.publish).toHaveBeenCalledWith('TOGGLE_WORKSPACE_PANEL');
   });
-  
+
   it('should render the grid', function() {
     var maxRows = workspacePanelSettings.maxRows;
     var maxColumns = workspacePanelSettings.maxColumns;
@@ -59,6 +90,30 @@ describe('WorkspacePanel', function() {
     var gridItems = this.viewerDiv.find('.grid-item');
     expect(gridItems.length).toBe(maxRows * maxColumns);
     expect(jQuery(gridItems[gridItems.length - 1]).data('gridstring')).toBe(maxGridString);
+  });
+
+  it('should set panel visibility', function() {
+    spyOn(this.panel, 'show');
+    spyOn(this.panel, 'hide');
+
+    this.panel.onPanelVisible(null, true);
+    expect(this.panel.show).toHaveBeenCalled();
+    expect(this.panel.hide).not.toHaveBeenCalled();
+    
+    this.panel.show.calls.reset();
+    this.panel.hide.calls.reset();
+
+    this.panel.onPanelVisible(null, false);
+    expect(this.panel.show).not.toHaveBeenCalled();
+    expect(this.panel.hide).toHaveBeenCalled();
+  });
+
+  ['hide', 'show'].forEach(function(action) {
+    it('should ' + action, function() {
+      spyOn(jQuery.fn, action);
+      this.panel[action]();
+      expect(jQuery.fn[action]).toHaveBeenCalled();
+    });
   });
 
 });
