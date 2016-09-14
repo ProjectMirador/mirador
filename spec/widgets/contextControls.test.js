f4a1d6b8c4ad0977cdc46a3475c93eeddfe0aaa1
aeschylus <scipioaffricanus@gmail.com>
2016-09-14 05:55:22 -0700

    add spot test for image manipulation initialisation

--- spec/widgets/contextControls.test.js
+++ spec/widgets/contextControls.test.js
@@ -26,6 +26,45 @@ describe('ContextControls', function() {
 
   });
 
+  describe('Manipulation Controls', function() {
+    beforeAll(function(){
+      this.container = jQuery('<div><div class="mirador-manipulation-controls"></div></div>');
+      jasmine.getFixtures().set(this.container);
+    });
+    it('should add manipulation controls', function() {
+      spyOn(Mirador.ContextControls.prototype,'init');
+      // spyOn(Mirador.ContextControls.prototype,'manipulationTemplate');
+      var canvasControls = {
+        "annotations" : {
+          "annotationLayer" : true,
+          "annotationCreation" : true,
+          "annotationState" : 'annoOff',
+          "annotationRefresh" : false
+        },
+        "imageManipulation" : {
+          "manipulationLayer" : true,
+          "controls" : {
+            "rotate" : true,
+            "brightness" : true,
+            "contrast" : true,
+            "saturate" : true,
+            "grayscale" : true,
+            "invert" : true
+          }
+        }
+      };
+      var contextControls = new Mirador.ContextControls({
+        container:this.container,
+        availableAnnotationStylePickers:['StrokeType'],
+        canvasControls: canvasControls
+      });
+
+      expect(contextControls.init).toHaveBeenCalled();
+      // expect(contextControls.manipulationTemplate).toHaveBeenCalled();
+      // expect(this.container.find('.mirador-osd-rotate-left')).toExist();
+    });
+  });
+
   describe('AnnotationStylePickers', function() {
 
     beforeAll(function(){
@@ -61,7 +100,7 @@ describe('ContextControls', function() {
       });
 
       expect(contextControls.addStrokeStylePicker).toHaveBeenCalled();
-    })
+    });
 
   });
 
