b5dee6194471f29fee654367d0ec5e02b32dec0b
aeschylus <scipioaffricanus@gmail.com>
2016-09-13 17:17:22 -0700

    added modernizr build for feature detection on CSS Filters

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,8 @@ module.exports = function(grunt) {
     'js/lib/isfahan.js',
     'js/lib/paper-full.min.js',
     'js/lib/spectrum.js',
-    'js/lib/i18next.min.js'
+    'js/lib/i18next.min.js',
+    'js/lib/modernizr.custom.js'
   ],
 
   // libraries/plugins for running tests
