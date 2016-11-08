cb5b2dbf4aa97a80754c9fd030c106fe1fe7222a
Jack Reed <phillipjreed@gmail.com>
2016-11-08 14:09:03 -0500

    Instead of using this, use explicitly defined variables

--- spec/mirador.test.js
+++ spec/mirador.test.js
@@ -1,12 +1,14 @@
 describe('Mirador | mirador.js', function() {
-  function startMirador(done, testContext) {
+  var div;
+  var mirador;
+  function startMirador(done) {
     localStorage.clear();
 
-    testContext.viewerDiv = jQuery('<div/>');
-    testContext.viewerDiv.attr('id', 'viewer');
-    testContext.viewerDiv.appendTo('body');
+    div = jQuery('<div/>');
+    div.attr('id', 'viewer');
+    div.appendTo('body');
 
-    testContext.mirador = Mirador({
+    mirador = Mirador({
       id: 'viewer',
       layout: "1x2",
       buildPath: 'spec/',
@@ -18,7 +20,7 @@ describe('Mirador | mirador.js', function() {
       }]
     });
 
-    testContext.mirador.viewer.eventEmitter.subscribe('manifestListItemRendered', function() {
+    mirador.viewer.eventEmitter.subscribe('manifestListItemRendered', function() {
       done();
     });
   }
@@ -26,41 +28,41 @@ describe('Mirador | mirador.js', function() {
   // Tests for object initialization
   describe('Initialization', function() {
     beforeAll(function(done) {
-      startMirador(done, this);
+      startMirador(done);
     });
     afterAll(function() {
       jQuery(window).unbind('resize');
       d3.select(window).on('resize', null);
-      this.viewerDiv.remove();
-      delete this.mirador;
+      div.remove();
+      mirador = null;
     });
 
     it('should render manifest list items', function() {
-      expect(this.mirador.viewer.manifestsPanel.manifestListItems.length).toBe(1);
+      expect(mirador.viewer.manifestsPanel.manifestListItems.length).toBe(1);
     });
 
     it('should start Mirador as blank workspace', function() {
-      expect(this.mirador.viewer).toBeDefined();
+      expect(mirador.viewer).toBeDefined();
     });
 
     it('should select a manifest', function() {
-      expect(this.viewerDiv.find('.addItemLink').first()).toExist();
-      this.viewerDiv.find('.addItemLink').first().click();
-      this.viewerDiv.find('.select-metadata').first().click();
-      expect(this.mirador.viewer.workspace.windows[0].manifest.uri).toEqual('spec/data/manifest.json');
+      expect(div.find('.addItemLink').first()).toExist();
+      div.find('.addItemLink').first().click();
+      div.find('.select-metadata').first().click();
+      expect(mirador.viewer.workspace.windows[0].manifest.uri).toEqual('spec/data/manifest.json');
     });
 
     it('should show logo specified in manifest', function(){
-      expect(this.viewerDiv.find(".repo-image>img")[0].getAttribute("src"))
+      expect(div.find(".repo-image>img")[0].getAttribute("src"))
         .toEqual("https://stacks-test.stanford.edu/image/iiif/ck546xs5106%2Folemiss1/full/300,/0/default.jpg");
     });
 
     it('should click first page in thumbnail view', function() {
-      expect(this.viewerDiv.find('.window').first()).toExist();
+      expect(div.find('.window').first()).toExist();
     });
 
     it('should click the next button', function() {
-      expect(this.viewerDiv.find('.window').first()).toExist();
+      expect(div.find('.window').first()).toExist();
     });
   });
 });
