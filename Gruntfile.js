2c9ca61264ec66c160823620ac3c3222ecaf1c3f
Radoslav Petkov <radipirate@gmail.com>
2016-12-07 11:24:21 +0200

    Update paper js version Add workaround for paper.js shape fill color because it does not detect mouse cursor on shapes with transparent fill color

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,7 @@ module.exports = function(grunt) {
     'js/lib/URI.min.js',
     'js/lib/mousetrap.min.js',
     'js/lib/isfahan.js',
-    'js/lib/paper-full.min.js',
+    'js/lib/paper-core.min.js',
     'js/lib/spectrum.js',
     'js/lib/i18next.min.js',
     'js/lib/modernizr.custom.js'
