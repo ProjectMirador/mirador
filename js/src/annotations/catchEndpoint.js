e76a1ce3692ec003cd4491ac46b5ab6aeb4e4734
Rashmi Singhal <rashmi.s.singhal@gmail.com>
2017-02-08 16:20:56 -0500

    Change miradorDualStrategy to account for multiple targets according to OA model.  Adjust localStorageEndpoint and catchEndpoint to handle these changes

--- js/src/annotations/catchEndpoint.js
+++ js/src/annotations/catchEndpoint.js
@@ -167,8 +167,10 @@
     //takes OA Annotation, gets Endpoint Annotation, and saves
     //if successful, MUST return the OA rendering of the annotation
     create: function(oaAnnotation, successCallback, errorCallback) {
-      var annotation = this.getAnnotationInEndpoint(oaAnnotation);
-      this.createCatchAnnotation(annotation, successCallback, errorCallback);
+      var annotations = this.getAnnotationInEndpoint(oaAnnotation);
+      annotations.forEach(function(annotation) {
+        this.createCatchAnnotation(annotation, successCallback, errorCallback);
+      });
     },
 
     createCatchAnnotation: function(catchAnnotation, successCallback, errorCallback) {
@@ -249,7 +251,10 @@
       } else {
         var value;
         motivation.push("oa:commenting");
-        if (typeof annotation.rangePosition === 'object') {
+        if (jQuery.isArray(annotation.rangePosition)) {
+          //dual strategy
+          on  = annotation.rangePosition;
+        } else if (typeof annotation.rangePosition === 'object') {
           //legacy strategy
           value = "xywh="+annotation.rangePosition.x+","+annotation.rangePosition.y+","+annotation.rangePosition.width+","+annotation.rangePosition.height;
           on = { "@type" : "oa:SpecificResource",
@@ -257,23 +262,6 @@
             "selector": {
                 "@type": "oa:FragmentSelector",
                 "value": value
-            }
-          };
-        } else if (annotation.bounds) {
-          //dual strategy
-          value = annotation.rangePosition;
-          on = { "@type" : "oa:SpecificResource",
-            "full" : annotation.uri,
-            "selector": {
-              "@type": "oa:Choice",
-              "default": {
-                "@type": "oa:FragmentSelector",
-                "value": "xywh=" + annotation.bounds.x + "," + annotation.bounds.y + "," + annotation.bounds.width + "," + annotation.bounds.height
-              },
-              "item": {
-                "@type": "oa:SvgSelector",
-                "value": value
-              }
             }
           };
         } else {
@@ -315,64 +303,82 @@
 
     // Converts OA Annotation to endpoint format
     getAnnotationInEndpoint: function(oaAnnotation) {
-      var annotation = {},
-      tags = [],
-      text;
-
-      if (oaAnnotation["@id"]) {
-        annotation.id = oaAnnotation["@id"];
-      }
-
-      annotation.media = "image";
-      jQuery.each(oaAnnotation.resource, function(index, value) {
-        if (value['@type'] === 'oa:Tag') {
-          tags.push(value.chars);
-        } else if (value['@type'] === 'dctypes:Text') {
-          text = value.chars;
+      var uris = [];
+      oaAnnotation.on.forEach(function(value) {
+        if (jQuery.inArray(value.full, uris) === -1) {
+          uris.push(value.full);
         }
       });
-      annotation.tags = tags;
-      annotation.text = text;
+      var annotations = [];
 
-      annotation.uri = oaAnnotation.on.full;
-      annotation.contextId = this.context_id;
-      annotation.collectionId = this.collection_id;
+      uris.forEach(function(uri) {
+        var annotation = {},
+        tags = [],
+        text;
 
-      var region = oaAnnotation.on.selector.item.value;
-      var regionArray;
-      //always assume dual strategy
-      if (region.indexOf('<svg') !== -1) {
-        //this is an svg string, so don't do anything special
-        annotation.rangePosition = region;
-      }
-      var coords = oaAnnotation.on.selector.default.value;
-      regionArray = coords.split('=')[1].split(',');
+        if (oaAnnotation["@id"]) {
+          annotation.id = oaAnnotation["@id"];
+        }
 
-      var canvas = this.imagesList[$.getImageIndexById(this.imagesList, oaAnnotation.on.full)];
-      var imageUrl = $.getThumbnailForCanvas(canvas, 300);
-      imageUrl = imageUrl.replace('full', regionArray.join(','));
-      annotation.thumb = imageUrl;
-      annotation.bounds = {"x":regionArray[0], "y":regionArray[1], "width":regionArray[2], "height":regionArray[3]};
+        annotation.media = "image";
+        jQuery.each(oaAnnotation.resource, function(index, value) {
+          if (value['@type'] === 'oa:Tag') {
+            tags.push(value.chars);
+          } else if (value['@type'] === 'dctypes:Text') {
+            text = value.chars;
+          }
+        });
+        annotation.tags = tags;
+        annotation.text = text;
 
-      annotation.updated = new Date().toISOString();
-      if (oaAnnotation.annotatedAt) {
-        annotation.created = oaAnnotation.annotatedAt;
-      } else {
-        annotation.created = annotation.updated;
-      }
-      // this needs to come from LTI annotation.user.id, annotation.user.name
-      annotation.user = {};
-      if (oaAnnotation.annotatedBy) {
-        annotation.user.name = oaAnnotation.annotatedBy.name;
-        annotation.user.id = oaAnnotation.annotatedBy['@id'];
-      } else {
-        annotation.user = this.catchOptions.user;
-      }
-      annotation.permissions = this.catchOptions.permissions;
-      annotation.archived = false;
-      annotation.ranges = [];
-      annotation.parent = "0";
-      return annotation;
+        annotation.uri = uri;
+        annotation.contextId = this.context_id;
+        annotation.collectionId = this.collection_id;
+        annotation.rangePosition = oaAnnotation.on;
+
+        var coordsArray = [];
+        oaAnnotation.on.forEach(function(value) {
+          coordsArray.push(value.selector.default.value.split('=')[1].split(','));
+        });
+        var left = coordsArray.map(function(i) {return i[0];});
+        var top = coordsArray.map(function(i) {return i[1];});
+        var right = coordsArray.map(function(i) {return i[0] + i[2];});
+        var bottom = coordsArray.map(function(i) {return i[1] + i[3];});
+
+        var newRect = {
+          "x" : Math.min.apply(null, left),
+          "y" : Math.min.apply(null, top),
+          "width" : Math.max.apply(null, right) - Math.min.apply(null, left),
+          "height" : Math.max.apply(null, bottom) - Math.min.apply(null, top)
+        };
+
+        var canvas = this.imagesList[$.getImageIndexById(this.imagesList, uri)];
+        var imageUrl = $.getThumbnailForCanvas(canvas, 300);
+        imageUrl = imageUrl.replace('full', newRect.x+','+newRect.y+','+newRect.width+','+newRect.height);
+        annotation.thumb = imageUrl;
+        annotation.bounds = {"x":newRect.x, "y":newRect.y, "width":newRect.width, "height":newRect.height};
+
+        annotation.updated = new Date().toISOString();
+        if (oaAnnotation.annotatedAt) {
+          annotation.created = oaAnnotation.annotatedAt;
+        } else {
+          annotation.created = annotation.updated;
+        }
+        // this needs to come from LTI annotation.user.id, annotation.user.name
+        annotation.user = {};
+        if (oaAnnotation.annotatedBy) {
+          annotation.user.name = oaAnnotation.annotatedBy.name;
+          annotation.user.id = oaAnnotation.annotatedBy['@id'];
+        } else {
+          annotation.user = this.catchOptions.user;
+        }
+        annotation.permissions = this.catchOptions.permissions;
+        annotation.archived = false;
+        annotation.ranges = [];
+        annotation.parent = "0";
+        annotations.push(annotation);
+      });
+      return annotations;
     }
   };
 
