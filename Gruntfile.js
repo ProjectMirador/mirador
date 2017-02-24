76925e80cc469fe2bc4a19dd017fbd2239823ac2
Jack Reed <phillipjreed@gmail.com>
2017-02-24 14:13:30 -0500

    move d3 to npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
     'js/lib/tinymce.min.js',
     'js/lib/handlebars.js',
     'js/lib/openseadragon.js',
-    'js/lib/d3.v3.min.js',
+    'node_modules/d3/d3.min.js',
     'js/lib/pubsub.min.js',
     'js/lib/URI.min.js',
     'js/lib/mousetrap.min.js',
