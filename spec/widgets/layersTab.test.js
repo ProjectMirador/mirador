e83acd48b2549abb76c0ba0acc6869d899f65490
aeschylus <scipioaffricanus@gmail.com>
2017-05-26 16:43:40 -0700

    add tests for layersTab event handlers

--- spec/widgets/layersTab.test.js
+++ spec/widgets/layersTab.test.js
@@ -34,23 +34,25 @@ describe('LayersTab', function() {
           },
           images: [
             {
+              id:1,
               url: 'aurl',
               getOpacity: function() {
                 return 0.3;
               },
               getVisible: function() {
-                return 'true';
+                return true;
               },
               getStatus: function() {
                 return 'drawn';
               }
             },
-            {url: 'aurl',
+            {id:2,
+              url: 'aurl',
              getOpacity: function() {
                return 1;
              },
              getVisible: function() {
-               return 'true';
+               return false;
              },
              getStatus: function() {
                return 'requested';
@@ -136,7 +138,7 @@ describe('LayersTab', function() {
 
   describe('tabStateUpdated', function() {
     beforeEach(function() {
-      spyOn(subject.eventEmitter, 'publish');
+      spyOn(subject.eventEmitter, 'publish').and.callThrough();
     });
     it('should set visible in the local state', function() {
       jQuery.each([false, true], function(k, visible) {
@@ -177,9 +179,157 @@ describe('LayersTab', function() {
     });
   });
 
-  //   xdescribe('bindEvents', function() {
+  describe('event callbacks', function() {
+    it('canvasIdUpdated', function() {
+      var mockCanvasId = 'https://oculus-dev.harvardx.harvard.edu/manifests/drs:5981093/canvas/canvas-5981096.json';
 
-  //   });
+      expect(subject.localState().canvasID).toEqual('https://oculus-dev.harvardx.harvard.edu/manifests/drs:5981093/canvas/canvas-5981094.json');
+      subject.canvasIdUpdated({}, mockCanvasId); // blank object represents event.
+      expect(subject.localState().canvasID).toEqual(mockCanvasId);
+    });
+
+    it('updateImageResourceStatus should show the resource status rendered appropriately', function() {
+      var mockImageResourceObject1 = {
+        id:2,
+        url: 'aurl',
+        getOpacity: function() {
+          return 1;
+        },
+        getVisible: function() {
+          return false;
+        },
+        getStatus: function() {
+          return 'loaded';
+        }
+      };
+      var mockImageResourceObject2 = {
+        id:2,
+        url: 'aurl',
+        getOpacity: function() {
+          return 1;
+        },
+        getVisible: function() {
+          return false;
+        },
+        getStatus: function() {
+          return 'requested';
+        }
+      };
+      var mockImageResourceObject3 = {
+        id:2,
+        url: 'aurl',
+        getOpacity: function() {
+          return 1;
+        },
+        getVisible: function() {
+          return false;
+        },
+        getStatus: function() {
+          return 'initialized';
+        }
+      };
+      var mockImageResourceObject4 = {
+        id:2,
+        url: 'aurl',
+        getOpacity: function() {
+          return 1;
+        },
+        getVisible: function() {
+          return false;
+        },
+        getStatus: function() {
+          return 'drawn';
+        }
+      };
+      expect(this.element.find('.layers-list-item').last()).toHaveClass('requested');
+      subject.updateImageResourceStatus({}, mockImageResourceObject1);
+      expect(this.element.find('.layers-list-item').last()).toHaveClass('loaded');
+
+      expect(this.element.find('.layers-list-item').last()).toHaveClass('loaded');
+      subject.updateImageResourceStatus({}, mockImageResourceObject2);
+      expect(this.element.find('.layers-list-item').last()).toHaveClass('requested');
+
+      expect(this.element.find('.layers-list-item').last()).toHaveClass('requested');
+      subject.updateImageResourceStatus({}, mockImageResourceObject3);
+      expect(this.element.find('.layers-list-item').last()).toHaveClass('initialized');
+
+      expect(this.element.find('.layers-list-item').last()).toHaveClass('initialized');
+      subject.updateImageResourceStatus({}, mockImageResourceObject4);
+      expect(this.element.find('.layers-list-item').last()).toHaveClass('drawn');
+    });
+
+    it('hideImageResource should set the visibility of the image resource to false', function() {
+      var mockImageResourceObject = {
+        id:1,
+        url: 'aurl',
+        getOpacity: function() {
+          return 0.3;
+        },
+        getVisible: function() {
+          return 'false';
+        },
+        getStatus: function() {
+          return 'drawn';
+        }
+      };
+      expect(this.element.find('.layers-listing li').first().find('.visibility-toggle')[0].checked).toBe(true);
+      subject.hideImageResource({}, mockImageResourceObject); // empty object mocks event object in callback
+      expect(this.element.find('.layers-listing li').first().find('.visibility-toggle')[0].checked).toBe(false);
+    });
+
+    it('showImageResource should set the visibility of the image resource to true', function() {
+      var mockImageResourceObject = {
+        id:2,
+        url: 'aurl',
+        getOpacity: function() {
+          return 0.3;
+        },
+        getVisible: function() {
+          return 'true';
+        },
+        getStatus: function() {
+          return 'drawn';
+        }
+      };
+      expect(this.element.find('.layers-listing li').last().find('.visibility-toggle')[0].checked).toBe(false);
+      expect(this.element.find('.layers-listing li').last().find('.opacity-label')).toHaveClass('disabled');
+      console.info(this.element.find('.layers-listing li').last().find('.opacity-label'));
+      subject.showImageResource({}, mockImageResourceObject); // empty object mocks event object in callback
+      expect(this.element.find('.layers-listing li').last().find('.opacity-label')).not.toHaveClass('disabled');
+      expect(this.element.find('.layers-listing li').last().find('.visibility-toggle')[0].checked).toBe(true);
+    });
+
+    it('updateImageResourceOpacity should adjust the value of the slider bar', function() {
+      var mockImageResourceObject = {
+        id:2,
+        url: 'aurl',
+        getOpacity: function() {
+          return 0.9;
+        },
+        getVisible: function() {
+          return 'true';
+        },
+        getStatus: function() {
+          return 'drawn';
+        }
+      };
+
+      expect(this.element.find('.layers-listing li').last().find('.opacity-slider').val()).toEqual('100');
+      subject.updateImageResourceOpacity({}, mockImageResourceObject); // empty object mocks event object in callback
+      expect(this.element.find('.layers-listing li').last().find('.opacity-slider').val()).toEqual('90');
+    });
+
+    it('imageFocusUpdated should display the disabled message and not the default template', function() {
+      expect(subject.localState().active).toBe(true);
+      subject.imageFocusUpdated('ThumbnailsView');
+      expect(subject.localState().active).toBe(false);
+      subject.imageFocusUpdated('ImageView');
+      expect(subject.localState().active).toBe(true);
+      subject.imageFocusUpdated('ScrollView');
+      expect(subject.localState().active).toBe(false);
+    });
+
+  });
 
   describe('render', function() {
     it('should hide itself if not visible', function() {
@@ -189,9 +339,8 @@ describe('LayersTab', function() {
         active: true,
         visible: false
       });
-      console.info(subject.canvasID);
-      console.info(subject.canvases);
       expect(subject.element.hide).toHaveBeenCalled();
+      expect(this.element.find('.layers-listing').children().length).toEqual(2);
     });
   });
 });
