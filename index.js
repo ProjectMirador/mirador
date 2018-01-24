0f75be072994e43c0dba437180bb6b15e69bb5ec
aeschylus <scipioaffricanus@gmail.com>
2018-01-24 11:31:32 -0800

    make useable in console

--- index.js
+++ index.js
@@ -37,22 +37,12 @@ function stateCore(state = initialState, action) {
 
 // Create a Redux store holding the state of your app.
 // Its API is { subscribe, dispatch, getState }.
-let store = createStore(stateCore)
+export const store = createStore(stateCore);
 
 // You can use subscribe() to update the UI in response to state changes.
 // Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
 // However it can also be handy to persist the current state in the localStorage.
 
-store.subscribe(() =>
-  console.log(store.getState())
-)
-
-// The only way to mutate the internal state is to dispatch an action.
-// The actions can be serialized, logged or stored and later replayed.
-// 0
-store.dispatch({ type: 'INCREMENT' })
-// 1
-store.dispatch({ type: 'INCREMENT' })
-// 2
-store.dispatch({ type: 'DECREMENT' })
-// 1
+// store.subscribe(() =>
+//   console.log(store.getState())
+// )
