310f8c027db4556d568e1ada2035793219c6dacc
Jack Reed <phillipjreed@gmail.com>
2018-02-21 18:32:45 -0700

    move default window properties to the action creator methdod

--- reducers/windows.js
+++ reducers/windows.js
@@ -1,17 +1,10 @@
 import ActionTypes from '../action-types';
 
-const initialWindowState = {
-  canvasIndex: 0
-};
-
 export const windowsReducer = (state = [], action) => {
   console.log('Reducing', action.type);
   switch (action.type) {
     case ActionTypes.ADD_WINDOW:
-      const window = {
-        id: `window.${new Date().valueOf()}`
-      };
-      return state.concat(Object.assign({}, initialWindowState, window));
+      return state.concat(Object.assign({}, action.payload));
     case ActionTypes.REMOVE_WINDOW:
       return state.filter(window => window.id !== action.windowId);
     case ActionTypes.NEXT_CANVAS:
