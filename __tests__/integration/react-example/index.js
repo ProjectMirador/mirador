06b109e1d37ddb143cd91a645d32a563f6e08cd2
aeschylus <scipioaffricanus@gmail.com>
2018-08-15 11:26:32 -0700

    add sass processing (#28)
    * add sass processing
    
    * fix style errors in config

--- __tests__/integration/react-example/index.js
+++ __tests__/integration/react-example/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import m3core from '../../../index.umd';
+import './index.scss';
 
 ReactDOM.render(
   <Provider store={m3core.store}>
--- integrations.config.js
+++ integrations.config.js
@@ -15,7 +15,14 @@ module.exports = [
           exclude: /(node_modules)/,
           loader: 'babel-loader',
         },
-      ],
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader', // creates style nodes from JS strings
+            'css-loader', // translates CSS into CommonJS
+            'sass-loader', // compiles Sass to CSS, using Node Sass by default
+          ],
+        }],
     },
   },
 ];
