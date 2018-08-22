8da23fa27532a294e71db48aae864764c3dc5bdd
Jack Reed <phillipjreed@gmail.com>
2018-08-22 12:26:42 -0600

    shuffle around scss and src directories

--- front-end.config.js
+++ front-end.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = [
   {
-    entry: './components/index.js',
+    entry: './src/index.js',
     output: {
       path: path.join(__dirname, 'dist'),
       filename: 'mirador.min.js',
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from '../components/App';
 import { store } from '../index';
-import './index.scss';
+import '../styles/index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
