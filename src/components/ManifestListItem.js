606d70337bab62182c64ee3fbf9d19f51151b741
Jack Reed <phillipjreed@gmail.com>
2018-09-26 13:12:23 -0600

    add docs + tests for new ManifestListItem component

--- src/components/ManifestListItem.js
+++ src/components/ManifestListItem.js
@@ -3,30 +3,52 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { actions } from '../store';
 
+
+/**
+ * Handling open button click
+ */
 const handleOpenButtonClick = (event, manifest, addWindow) => {
   addWindow({});
-}
+};
 /**
  * Represents an item in a list of currently-loaded or loading manifests
  * @param {object} props
  * @param {object} [props.manifest = string]
  */
-const ManifestListItem = ({ manifest, addWindow}) => (
+
+/**
+ * Determines which classes should be used for display, based on the state of
+ * the manifest
+ * @memberof ManifestListItem
+ * @private
+ */
+const ManifestListItem = ({ manifest, addWindow }) => (
   <li className="manifest-list-item">
-    <a href="#" onClick={(event) => handleOpenButtonClick(event, manifest, addWindow)}>
+    <button type="button" onClick={event => handleOpenButtonClick(event, manifest, addWindow)}>
       {manifest}
-    </a>
+    </button>
   </li>
 );
 
 ManifestListItem.propTypes = {
   manifest: PropTypes.string.isRequired, // eslint-disable-line react/forbid-prop-types
+  addWindow: PropTypes.func.isRequired,
 };
 
+/**
+ * mapStateToProps - to hook up connect
+ * @memberof ManifestListItem
+ * @private
+ */
 const mapStateToProps = () => (
   {}
 );
 
+/**
+ * mapDispatchToProps - used to hook up connect to action creators
+ * @memberof ManifestListItem
+ * @private
+ */
 const mapDispatchToProps = dispatch => ({
   addWindow: options => (
     dispatch(actions.addWindow(options))
