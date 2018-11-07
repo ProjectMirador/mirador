f19473a3337ba6534bf7ffd7f068961b18ec9797
Jack Reed <phillipjreed@gmail.com>
2018-11-07 12:41:08 -0700

    switch default size to 400px for thumb in window

--- __tests__/src/components/Window.test.js
+++ __tests__/src/components/Window.test.js
@@ -15,12 +15,12 @@ describe('Window', () => {
   });
 
   it('returns the width and height style attribute', () => {
-    expect(wrapper.instance().styleAttributes()).toEqual({ width: '200px', height: '200px' });
+    expect(wrapper.instance().styleAttributes()).toEqual({ width: '400px', height: '400px' });
   });
 
   it('renders without an error', () => {
-    expect(wrapper.find('.mirador-window').prop('style')).toHaveProperty('width', '200px');
-    expect(wrapper.find('.mirador-window').prop('style')).toHaveProperty('height', '200px');
+    expect(wrapper.find('.mirador-window').prop('style')).toHaveProperty('width', '400px');
+    expect(wrapper.find('.mirador-window').prop('style')).toHaveProperty('height', '400px');
     expect(wrapper.find('div.mirador-window').length).toBe(1);
     expect(wrapper.find('div.mirador-window h3').text()).toBe('Test 2 Manifest: Metadata Pairs');
     expect(wrapper.find('div.mirador-window img').prop('src')).toBe('http://placekitten.com/200/300');
