117412b9f1246c28e674825800c99bc6336b1ea4
Jack Reed <phillipjreed@gmail.com>
2018-11-27 17:10:07 -0700

    removes redux-logger as it was too busy

--- src/store.js
+++ src/store.js
@@ -4,21 +4,16 @@
 // (normalizer library)
 
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/index';
 import * as ActionCreators from './actions';
 
-const loggerMiddleware = createLogger();
 // Create a Redux store holding the state of your app.
 // Its API is { subscribe, dispatch, getState }.
 export const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware,
-  )),
+  composeWithDevTools(applyMiddleware(thunkMiddleware)),
 );
 
 export const actions = ActionCreators;
