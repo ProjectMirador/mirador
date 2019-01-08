f80aebd926ea08b1d8a49058527b39b3f62d3926
aeschylus <scipioaffricanus@gmail.com>
2019-01-07 16:16:49 -0800

    Type1 plugin save button (#95)
    * prototype a keyboard shortcuts plugin
    
    * separate library and instance API layers into separate files
    
    * fix lint errors
    
    * remove prototype save button plugin

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import deepmerge from 'deepmerge';
-import App from './components/App';
-import { actions, store } from './store';
-import settings from './config/settings';
-import './styles/index.scss';
+import init from './init';
 
-/**
- * Default Mirador instantiation
- */
-export default function Mirador(config) {
-  const action = actions.setConfig(deepmerge(settings, config));
-  store.dispatch(action);
-  ReactDOM.render(
-    <Provider store={store}>
-      <App config={config} />
-    </Provider>,
-    document.getElementById(config.id),
-  );
-  return {
-    actions,
-    store,
-  };
-}
+const exports = {
+  viewer: init,
+  plugins: [],
+};
+
+export default exports;
