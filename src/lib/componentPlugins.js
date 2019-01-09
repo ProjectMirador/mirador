3f48db8e41ef89c76e6960e28187dd405e99c495
Jack Reed <phillipjreed@gmail.com>
2019-01-08 18:15:49 -0700

    setup plugins so that mapstate and mapdispatch work, also give them access to config

--- src/lib/componentPlugins.js
+++ src/lib/componentPlugins.js
@@ -1,16 +1,14 @@
 /**
  * componentPlugins - gets window plugins based on a component parent
  */
-export default function componentPlugins(componentName) {
+export default function componentPlugins(componentName, plugins = []) {
   // TODO: Figure out how to handle when not running under window. Probably not
   // a pressing priority, but relevant for tests
-  if (!window.Mirador) return [];
-  return Object.keys(window.Mirador.plugins)
-    .reduce((result, component) => {
-      // TODO: add a condition to only grab plugins from the config
-      if (window.Mirador.plugins[component].parent === componentName) {
-        result.push(window.Mirador.plugins[component]);
-      }
-      return result;
-    }, []);
+  return plugins.map((pluginName) => {
+    if (window.Mirador.plugins[pluginName]
+        && window.Mirador.plugins[pluginName].parent === componentName) {
+      return window.Mirador.plugins[pluginName];
+    }
+    return null;
+  }).filter(plugin => (plugin !== (undefined || null)));
 }
--- src/lib/miradorWithPlugins.js
+++ src/lib/miradorWithPlugins.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import componentPlugins from './componentPlugins';
 /**
  * miradorWithPlugins - renders and returns a component with provided plugins
@@ -6,7 +8,9 @@ import componentPlugins from './componentPlugins';
  * @param  {type} WrappedComponent
  */
 export default function miradorWithPlugins(WrappedComponent) {
-  return class extends Component {
+  /**
+   */
+  class ConnectedComponent extends Component {
     /**
      * constructor -
      */
@@ -27,15 +31,30 @@ export default function miradorWithPlugins(WrappedComponent) {
      * render - renders the wrapped component with the plugins.
      */
     render() {
+      const { config } = this.props;
+      const { plugins } = config;
       return (
         <Fragment>
           <WrappedComponent {...this.props} ref={(parent) => { this.pluginParent = parent; }} />
-          {componentPlugins(WrappedComponent.name).map(component => React.createElement(
-            component.component,
-            { key: component.name, ...this.props, pluginParent: this.pluginParent },
-          ))}
+          { /* TODO: Refactor .name here in some way so we dont need to rely on it */}
+          {componentPlugins(WrappedComponent.name, plugins)
+            .map(component => React.createElement(
+              connect(component.mapStateToProps, component.mapDispatchToProps)(component.component),
+              { key: component.name, ...this.props, pluginParent: this.pluginParent },
+            ))
+          }
         </Fragment>
       );
     }
+  }
+
+  ConnectedComponent.propTypes = {
+    config: PropTypes.instanceOf(Object).isRequired,
   };
+
+  /**
+   */
+  const mapStateToProps = state => ({ config: state.config });
+
+  return connect(mapStateToProps, null)(ConnectedComponent);
 }
