23a588d9dede57731635bfb30923011d31ab63fa
Andrew Winget (Standard) <scipioaffricanus@gmail.com>
2019-01-11 04:12:04 -0800

    open reducers and actions to modification by plugins

--- src/init.js
+++ src/init.js
@@ -3,18 +3,55 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import deepmerge from 'deepmerge';
 import App from './components/App';
-import { actions, store } from './store';
+import createRootReducer from './reducers/index';
+import { store, actions } from './store';
 import settings from './config/settings';
 import './styles/index.scss';
+
+/**
+ * Process Plugins
+ */
+const processPlugins = (plugins = []) => {
+  const actionCreators = [];
+  const reducers = [];
+
+  plugins.forEach((pluginName) => {
+    const plugin = window.Mirador.plugins[pluginName];
+
+    // Add Actions
+    if (plugin.actions) {
+      Object.keys(plugin.actions)
+        .forEach(actionName => actionCreators.push({
+          name: actionName,
+          action: plugin.actions[actionName],
+        }));
+    }
+    // Add Reducers
+    if (plugin.reducers) {
+      Object.keys(plugin.reducers)
+        .forEach(reducerName => reducers.push({
+          name: reducerName,
+          reducer: plugin.reducers[reducerName],
+        }));
+    }
+  });
+
+  actionCreators.forEach((action) => { actions[action.name] = action.action; });
+  reducers.forEach((reducer) => { store.pluginReducers[reducer.name] = reducer.reducer; });
+  store.replaceReducer(createRootReducer(store.pluginReducers));
+};
 
 /**
  * Default Mirador instantiation
  */
 export default function (config) {
+  processPlugins(config.plugins);
+
   const viewer = {
     actions,
     store,
   };
+
   const action = actions.setConfig(deepmerge(settings, config));
   store.dispatch(action);
 
--- src/store.js
+++ src/store.js
@@ -6,15 +6,24 @@
 import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers/index';
+import createRootReducer from './reducers/index';
 import * as ActionCreators from './actions';
+
+
+/**
+ * Configure Store
+ */
+export function configureStore() {
+  const store = createStore(
+    createRootReducer(),
+    composeWithDevTools(applyMiddleware(thunkMiddleware)),
+  );
+  store.pluginReducers = {};
+  return store;
+}
 
 // Create a Redux store holding the state of your app.
 // Its API is { subscribe, dispatch, getState }.
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunkMiddleware)),
-);
-
+export const store = configureStore();
 export const actions = ActionCreators;
 export default { actions, store };
