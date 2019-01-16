f1bd951d3fc30a6cd0914696c9a0fd8e99f7cafc
Johannes Baiter <johannes.baiter@gmail.com>
2019-01-16 10:49:38 -0800

    Replace Object.assign with the spread operator

--- src/reducers/infoResponses.js
+++ src/reducers/infoResponses.js
@@ -6,25 +6,23 @@ import ActionTypes from '../action-types';
 const infoResponsesReducer = (state = {}, action) => {
   switch (action.type) {
     case ActionTypes.REQUEST_INFO_RESPONSE:
-      return Object.assign({}, state, {
-        [action.infoId]: {
-          isFetching: true,
-        },
-      });
+      return { ...state, [action.infoId]: { isFetching: true } };
     case ActionTypes.RECEIVE_INFO_RESPONSE:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         [action.infoId]: {
           json: action.infoJson,
           isFetching: false,
         },
-      });
+      };
     case ActionTypes.RECEIVE_INFO_RESPONSE_FAILURE:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         [action.infoId]: {
           error: action.error,
           isFetching: false,
         },
-      });
+      };
     case ActionTypes.REMOVE_INFO_RESPONSE:
       return Object.keys(state).reduce((object, key) => {
         if (key !== action.infoId) {
--- src/reducers/windows.js
+++ src/reducers/windows.js
@@ -6,20 +6,20 @@ import ActionTypes from '../action-types';
 const windowsReducer = (state = [], action) => {
   switch (action.type) {
     case ActionTypes.ADD_WINDOW:
-      return state.concat(Object.assign({}, action.payload));
+      return state.concat({ ...action.payload });
     case ActionTypes.REMOVE_WINDOW:
       return state.filter(window => window.id !== action.windowId);
     case ActionTypes.NEXT_CANVAS:
       return state.map((window) => {
         if (window.id === action.windowId) {
-          return Object.assign({}, window, { canvasIndex: window.canvasIndex + 1 });
+          return { ...window, canvasIndex: window.canvasIndex + 1 };
         }
         return window;
       });
     case ActionTypes.PREVIOUS_CANVAS:
       return state.map((window) => {
         if (window.id === action.windowId) {
-          return Object.assign({}, window, { canvasIndex: window.canvasIndex - 1 });
+          return { ...window, canvasIndex: window.canvasIndex - 1 };
         }
         return window;
       });
