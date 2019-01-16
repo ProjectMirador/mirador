d5db01968db72cd7ed605c58371ae019f210e7b3
mikeapp <michael.appleby@yale.edu>
2019-01-15 16:15:38 -0800

    Incorrect labels for infoResponse tests. Closes #1607

--- __tests__/src/actions/infoResponse.test.js
+++ __tests__/src/actions/infoResponse.test.js
@@ -42,12 +42,8 @@ describe('infoResponse actions', () => {
       beforeEach(() => {
         fetch.mockResponseOnce(JSON.stringify({ data: '12345' })); // eslint-disable-line no-undef
       });
-      it('dispatches the REQUEST_MANIFEST action', () => {
-        store.dispatch(
-          actions.fetchInfoResponse(
-            'https://stacks.stanford.edu/image/iiif/sn904cj3429%2F12027000/info.json'
-          )
-        );
+      it('dispatches the REQUEST_INFO_RESPONSE action', () => {
+        store.dispatch(actions.fetchInfoResponse('https://stacks.stanford.edu/image/iiif/sn904cj3429%2F12027000/info.json'));
         expect(store.getActions()).toEqual([
           {
             infoId: 'https://stacks.stanford.edu/image/iiif/sn904cj3429%2F12027000/info.json',
@@ -55,13 +51,8 @@ describe('infoResponse actions', () => {
           }
         ]);
       });
-      it('dispatches the REQUEST_MANIFEST and then RECEIVE_MANIFEST', () => {
-        store
-          .dispatch(
-            actions.fetchInfoResponse(
-              'https://stacks.stanford.edu/image/iiif/sn904cj3429%2F12027000/info.json'
-            )
-          )
+      it('dispatches the REQUEST_INFO_RESPONSE and then RECEIVE_INFO_RESPONSE', () => {
+        store.dispatch(actions.fetchInfoResponse('https://stacks.stanford.edu/image/iiif/sn904cj3429%2F12027000/info.json'))
           .then(() => {
             const expectedActions = store.getActions();
             expect(expectedActions).toEqual([
