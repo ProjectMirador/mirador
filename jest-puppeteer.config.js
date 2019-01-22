e9f21c16ea51c42a5fa3fa9a37d0f3562d8e912a
Jack Reed <phillipjreed@gmail.com>
2019-01-22 15:12:28 -0700

    Update json-server startup/shutdown to prevent rouge processes

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,8 +2,11 @@ module.exports = {
   launch: {
     headless: process.env.HEADLESS !== 'false',
   },
-  server: {
+  server: [{
     command: 'npm run server -- -p 4488',
     port: 4488,
   },
+  {
+    command: 'npm run server:json',
+  }],
 };
