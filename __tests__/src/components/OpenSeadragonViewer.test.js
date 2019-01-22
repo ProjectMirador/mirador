0edf48e523f7d42bc413d04ef3b048c9f0713d29
Jack Reed <phillipjreed@gmail.com>
2019-01-22 16:38:21 -0700

    Zoom OSD canvas based off of the first tilesource passed in

--- __tests__/src/components/OpenSeadragonViewer.test.js
+++ __tests__/src/components/OpenSeadragonViewer.test.js
@@ -23,4 +23,34 @@ describe('OpenSeadragonViewer', () => {
       expect(wrapper.instance().tileSourcesMatch([{ '@id': 'http://foo' }])).toBe(true);
     });
   });
+  describe('addTileSource', () => {
+    it('calls addTiledImage asynchronously on the OSD viewer', async () => {
+      wrapper.instance().viewer = {
+        addTiledImage: jest.fn().mockResolvedValue('event'),
+      };
+      wrapper.instance().addTileSource({}).then((event) => {
+        expect(event).toBe('event');
+      });
+    });
+    it('when a viewer is not available, returns an unresolved Promise', () => {
+      expect(wrapper.instance().addTileSource({})).toEqual(expect.any(Promise));
+    });
+  });
+  describe('fitBounds', () => {
+    it('calls OSD viewport.fitBounds with provided x, y, w, h', () => {
+      wrapper.instance().viewer = {
+        viewport: {
+          fitBounds: jest.fn(),
+          imageToViewportRectangle: jest.fn(),
+        },
+      };
+      wrapper.instance().fitBounds(1, 2, 3, 4);
+      expect(
+        wrapper.instance().viewer.viewport.imageToViewportRectangle,
+      ).toHaveBeenCalledWith(1, 2, 3, 4);
+      expect(
+        wrapper.instance().viewer.viewport.fitBounds,
+      ).toHaveBeenCalled();
+    });
+  });
 });
