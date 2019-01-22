2c619b4031ee1d3d4795f82d40c7f6ec0ce2e788
Jack Reed <phillipjreed@gmail.com>
2019-01-22 12:24:37 -0700

    Comment no longer valid resolved by @charbugs in #1669

--- src/actions/window.js
+++ src/actions/window.js
@@ -19,8 +19,6 @@ export function focusWindow(windowId) {
  */
 export function addWindow(options) {
   const defaultOptions = {
-    // TODO: Windows should be a hash with id's as keys for easy lookups
-    // https://redux.js.org/faq/organizing-state#how-do-i-organize-nested-or-duplicate-data-in-my-state
     id: `window-${uuid()}`,
     canvasIndex: 0,
     collectionIndex: 0,
