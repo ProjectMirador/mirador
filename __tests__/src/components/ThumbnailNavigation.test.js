acc9e915c23bf89bdfb088645f02ba0e300eb1c9
Jack Reed <phillipjreed@gmail.com>
2019-01-25 12:35:30 -0700

    sets up react-virtualized for high-performant thumb navigation

--- __tests__/src/components/ThumbnailNavigation.test.js
+++ __tests__/src/components/ThumbnailNavigation.test.js
@@ -5,6 +5,7 @@ import { ThumbnailNavigation } from '../../../src/components/ThumbnailNavigation
 describe('ThumbnailNavigation', () => {
   let wrapper;
   let setCanvas;
+  let renderedGrid;
   beforeEach(() => {
     setCanvas = jest.fn();
     wrapper = shallow(
@@ -12,9 +13,15 @@ describe('ThumbnailNavigation', () => {
         canvases={[
           {
             index: 0,
+            getHeight: () => (1000),
+            getWidth: () => (2000),
+            getCanonicalImageUri: () => ('http://imageuri'),
           },
           {
             index: 1,
+            getHeight: () => (1000),
+            getWidth: () => (2000),
+            getCanonicalImageUri: () => ('http://imageuri'),
           },
         ]}
         window={{
@@ -26,18 +33,26 @@ describe('ThumbnailNavigation', () => {
         setCanvas={setCanvas}
       />,
     );
+    renderedGrid = wrapper.find('AutoSizer')
+      .dive()
+      .find('Grid')
+      .dive();
   });
   it('renders the component', () => {
     expect(wrapper.find('.mirador-thumb-navigation').length).toBe(1);
   });
   it('renders li elements based off of number of canvases', () => {
-    expect(wrapper.find('.mirador-thumbnail-nav-canvas').length).toBe(2);
+    expect(renderedGrid.find('.mirador-thumbnail-nav-canvas').length).toBe(2);
   });
   it('sets a mirador-current-canvas class on current canvas', () => {
     expect(wrapper.find('.mirador-thumbnail-nav-canvas-1.mirador-current-canvas'));
   });
   it('when clicked, updates the current canvas', () => {
-    wrapper.find('.mirador-thumbnail-nav-canvas-0').simulate('click');
+    renderedGrid.find('.mirador-thumbnail-nav-canvas-0').simulate('click');
     expect(setCanvas).toHaveBeenCalledWith('foobar', 0);
+  });
+  it('sets up calculated width based off of height of area and dimensions of canvas', () => {
+    expect(renderedGrid.find('.mirador-thumbnail-nav-container').first().prop('style').width).toEqual(308);
+    expect(renderedGrid.find('.mirador-thumbnail-nav-canvas').first().prop('style').width).toEqual(300);
   });
 });
