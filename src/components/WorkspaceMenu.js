1d9582c7eed4ab0c7c8d3291a9cd9fe79e697a3b
Chris Beer <cabeer@stanford.edu>
2019-01-29 13:39:31 -0800

    Add 'List all open windows' menu item

--- src/components/WorkspaceMenu.js
+++ src/components/WorkspaceMenu.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { compose } from 'redux';
 import Menu from '@material-ui/core/Menu';
-// import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
+import ViewHeadlineIcon from '@material-ui/icons/ViewHeadline';
 import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -16,7 +19,14 @@ export class WorkspaceMenu extends Component {
   render() {
     const { handleClose, anchorEl } = this.props;
     return (
-      <Menu id="workspace-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose} />
+      <Menu id="workspace-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+        <MenuItem>
+          <ListItemIcon>
+            <ViewHeadlineIcon />
+          </ListItemIcon>
+          <Typography varient="inherit">List all open windows</Typography>
+        </MenuItem>
+      </Menu>
     );
   }
 }
