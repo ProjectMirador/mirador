b850c0ed4431418aafc646d1ce8071d9638b7662
Jack Reed <phillipjreed@gmail.com>
2019-01-31 09:04:44 -0700

    Add IntersectionObserver polyfill for Safari fixes #1769

--- src/components/CanvasThumbnail.js
+++ src/components/CanvasThumbnail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import 'intersection-observer'; // polyfill needed for Safari
 import IntersectionObserver from '@researchgate/react-intersection-observer';
 
 /**
