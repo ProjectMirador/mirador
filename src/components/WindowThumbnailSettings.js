fc4745ed43d64557a0673e24c4e8fbd6e32055ca
Chris Beer <cabeer@stanford.edu>
2019-01-31 15:01:18 -0800

    Replace radio buttons with icons

--- src/components/WindowThumbnailSettings.js
+++ src/components/WindowThumbnailSettings.js
@@ -3,6 +3,8 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Typography from '@material-ui/core/Typography';
+import CancelPresentationIcon from '@material-ui/icons/CancelPresentation';
+import VideoLabelIcon from '@material-ui/icons/VideoLabel';
 import PropTypes from 'prop-types';
 
 /**
@@ -42,19 +44,19 @@ export default class WindowThumbnailSettings extends Component {
         <RadioGroup aria-label="position" name="position" value={thumbnailNavigationPosition} onChange={this.handleChange} row>
           <FormControlLabel
             value="off"
-            control={<Radio color="primary" />}
+            control={<Radio color="primary" icon={<CancelPresentationIcon />} checkedIcon={<CancelPresentationIcon />} />}
             label="Off"
             labelPlacement="bottom"
           />
           <FormControlLabel
             value="bottom"
-            control={<Radio color="primary" />}
+            control={<Radio color="primary" icon={<ThumbnailNavigationBottomIcon />} checkedIcon={<ThumbnailNavigationBottomIcon />} />}
             label="Bottom"
             labelPlacement="bottom"
           />
           <FormControlLabel
             value="right"
-            control={<Radio color="primary" />}
+            control={<Radio color="primary" icon={<ThumbnailNavigationRightIcon />} checkedIcon={<ThumbnailNavigationRightIcon />} />}
             label="Right"
             labelPlacement="bottom"
           />
@@ -62,6 +64,24 @@ export default class WindowThumbnailSettings extends Component {
       </>
     );
   }
+}
+
+/**
+ * @private
+ */
+function ThumbnailNavigationBottomIcon(props) {
+  return (
+    <VideoLabelIcon />
+  );
+}
+
+/**
+ * @private
+ */
+function ThumbnailNavigationRightIcon(props) {
+  return (
+    <VideoLabelIcon style={{ transform: 'rotate(-90deg)' }} />
+  );
 }
 
 WindowThumbnailSettings.propTypes = {
