ecc1804ef066618f73df1343e7397e6cc24fd3d9
Glenn Fischer <gfischer@ub.uni-leipzig.de>
2019-02-18 16:45:32 +0100

    #1773: refactor test

--- __tests__/integration/mirador/window_sidebar.test.js
+++ __tests__/integration/mirador/window_sidebar.test.js
@@ -3,16 +3,16 @@
 describe('Window Sidebars', () => {
   beforeAll(async () => {
     await page.goto('http://127.0.0.1:4488/__tests__/integration/mirador/');
-  });
 
-  it('renders and updates canvas level metadata', async () => {
     await expect(page).toClick('#addBtn');
     await expect(page).toFill('#manifestURL', 'http://localhost:5000/api/001');
     await expect(page).toClick('#fetchBtn');
 
     await expect(page).toMatchElement('[data-manifestid="http://localhost:5000/api/001"] button');
     await expect(page).toClick('[data-manifestid="http://localhost:5000/api/001"] button');
+  });
 
+  it('renders and updates canvas level metadata', async () => {
     await expect(page).toMatchElement(
       'h3',
       { text: 'Bodleian Library Human Freaks 2 (33)' },
@@ -33,14 +33,6 @@ describe('Window Sidebars', () => {
   });
 
   it('renders canvas navigation and updates canvas after clicking a navigation item', async () => {
-    await expect(page).toClick('#addBtn');
-    await expect(page).toFill('#manifestURL', 'http://localhost:5000/api/001');
-    await expect(page).toClick('#fetchBtn');
-    // TODO: Refactor the app so we get rid of the wait
-    await page.waitFor(1000);
-    await expect(page).toMatchElement('li', { text: 'http://localhost:5000/api/001' });
-    await expect(page).toClick('li button', { text: 'http://localhost:5000/api/001' });
-
     const windows = await page.evaluate(() => (
       miradorInstance.store.getState().windows
     ));
