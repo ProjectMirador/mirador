949729574fffc7bcfd2e4013cd914ddc2cf727ab
Mathias Maaß <mathias.maass@uni-leipzig.de>
2019-02-19 09:56:01 +0100

    Create <SanitizedHtml/> component. Part of #1854

---
+++ src/lib/htmlRules.js
@@ -0,0 +1,7 @@
+
+const basic = {
+  ALLOWED_TAGS: ['a', 'b', 'br', 'i', 'img', 'p', 'span', 'strong', 'em', 'ul', 'ol', 'li'],
+  ALLOWED_ATTR: ['href', 'target', 'src', 'alt', 'dir'],
+};
+
+export default { basic };
