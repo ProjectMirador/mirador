24de5151392b0fd637a3873b2dbc91372003ec59
Jessie Keck <jessie.keck@gmail.com>
2019-02-19 15:03:13 -0800

    Add ManifestListItemError presentational and container components.

---
+++ __tests__/src/components/ManifestListItemError.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Typography from '@material-ui/core/Typography';
+import ManifestListItemError from '../../../src/components/ManifestListItemError';
+
+/**
+ * Helper function to wrap creating a ManifestListItemError component
+*/
+function createWrapper(props) {
+  return shallow(
+    <ManifestListItemError
+      manifestId="http://example.com"
+      onDismissClick={() => {}}
+      onTryAgainClick={() => {}}
+      t={key => key}
+      {...props}
+    />,
+  ).dive(); // unwrap HOC created by withStyles()
+}
+
+describe('ManifestListItemError', () => {
+  let wrapper;
+  let mockFn;
+
+  it('renders the failed manifest url and error key', () => {
+    wrapper = createWrapper();
+
+    expect(
+      wrapper.find(Typography).children().first().text(),
+    ).toEqual('manifestError'); // the i18n key
+
+    expect(
+      wrapper.find(Typography).children().last().text(),
+    ).toEqual('http://example.com');
+  });
+
+  it('has a dismiss button that fires the onDismissClick prop', () => {
+    mockFn = jest.fn();
+    wrapper = createWrapper({ onDismissClick: mockFn });
+
+    wrapper.find('WithStyles(Button)').first().simulate('click');
+    expect(mockFn).toHaveBeenCalledTimes(1);
+    expect(mockFn).toHaveBeenCalledWith('http://example.com');
+  });
+
+  it('has a try again button that fires the onTryAgainClick prop', () => {
+    mockFn = jest.fn();
+    wrapper = createWrapper({ onTryAgainClick: mockFn });
+
+    wrapper.find('WithStyles(Button)').last().simulate('click');
+    expect(mockFn).toHaveBeenCalledTimes(1);
+    expect(mockFn).toHaveBeenCalledWith('http://example.com');
+  });
+});
