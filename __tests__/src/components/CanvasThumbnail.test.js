9ca7ed489b71343bf7391ec8c843040c06f7fc99
Glenn Fischer <gfischer@ub.uni-leipzig.de>
2019-02-20 16:53:18 +0100

    #1872: fixes tests

--- __tests__/src/components/CanvasThumbnail.test.js
+++ __tests__/src/components/CanvasThumbnail.test.js
@@ -9,21 +9,23 @@ describe('CanvasThumbnail', () => {
     wrapper = shallow(
       <CanvasThumbnail
         imageUrl="https://stacks.stanford.edu/image/iiif/sn904cj3429%2F12027000/full/193,/0/default.jpg"
+        onClick={() => {}}
       />,
     );
   });
+
   it('renders properly', () => {
     expect(wrapper.matchesElement(
-      <div>
-        <IntersectionObserver onChange={wrapper.instance().handleIntersection}>
-          <img alt="" height={150} />
-        </IntersectionObserver>
-      </div>,
+      <IntersectionObserver onChange={wrapper.instance().handleIntersection}>
+        <img alt="" />
+      </IntersectionObserver>,
     )).toBe(true);
   });
+
   it('defaults using the placeholder image', () => {
     expect(wrapper.find('img').props().src).toMatch(/data:image\/png;base64/);
   });
+
   it('when handleIntersection is called, loads the image', () => {
     wrapper.instance().handleIntersection({ isIntersecting: true });
     expect(wrapper.find('img').props().src).toMatch(/stacks/);
