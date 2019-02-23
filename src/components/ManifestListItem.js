a1cf2b004f3f7292eeeabb4c299af156dcf16b93
Jessie Keck <jessie.keck@gmail.com>
2019-02-22 16:49:13 -0800

    Add translation keys for hard-coded string in ManifestListItem

--- src/components/ManifestListItem.js
+++ src/components/ManifestListItem.js
@@ -116,7 +116,7 @@ class ManifestListItem extends React.Component {
             </Grid>
             <Grid item xs={8} sm={4}>
               <Typography className={ns('manifest-list-item-provider')}>{provider || t('addedFromUrl')}</Typography>
-              <Typography>{`${size} items`}</Typography>
+              <Typography>{t('numItems', { number: size })}</Typography>
             </Grid>
 
             <Grid item xs={4} sm={2}>
