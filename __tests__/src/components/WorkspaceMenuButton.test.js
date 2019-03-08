8427e599bd3549863b973ddf67647a058b3b8ca6
Jessie Keck <jessie.keck@gmail.com>
2019-03-07 20:26:03 -0800

    Align the WorkspaceMenu and add the selected state styling to the WorkspaceMenuButton when the menu is open.

--- __tests__/src/components/WorkspaceMenuButton.test.js
+++ __tests__/src/components/WorkspaceMenuButton.test.js
@@ -6,7 +6,7 @@ describe('WorkspaceMenuButton', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <WorkspaceMenuButton classes={{}} />,
+      <WorkspaceMenuButton classes={{ ctrlBtnSelected: 'ctrlBtnSelected' }} />,
     );
   });
 
@@ -16,5 +16,13 @@ describe('WorkspaceMenuButton', () => {
   it('when clicked, updates the state', () => {
     wrapper.find('WithStyles(IconButton)').simulate('click', {});
     // TODO: this is currently a no-op
+  });
+
+  it('the button has a class indicating that it is "selected" once it is clicked', () => {
+    expect(wrapper.find('.ctrlBtnSelected').length).toBe(0);
+    wrapper.find('WithStyles(IconButton)').simulate('click', { currentTarget: 'anElement' });
+    expect(wrapper.find('.ctrlBtnSelected').length).toBe(1);
+    wrapper.find('WithStyles(IconButton)').simulate('click', {});
+    expect(wrapper.find('.ctrlBtnSelected').length).toBe(0);
   });
 });
