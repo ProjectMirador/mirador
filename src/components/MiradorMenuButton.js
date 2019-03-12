1527e11c29e16dab755a4bbaf7c3393f55ef0dfe
Jessie Keck <jessie.keck@gmail.com>
2019-03-12 12:36:19 -0700

    Add a MiradorMenuButton component to handle button tooltips/labels/etc.

---
+++ src/components/MiradorMenuButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+
+
+/**
+ * MiradorMenuButton ~ Wrap the given icon prop in an IconButton and a Tooltip.
+ * This shares the passed in aria-label w/ the Tooltip (as title) and the IconButton
+ * All props besides icon are spread to the IconButton component
+*/
+export function MiradorMenuButton(props) {
+  const { 'aria-label': ariaLabel } = props;
+  const { icon, ...iconButtonProps } = props;
+
+  return (
+    <Tooltip title={ariaLabel}>
+      {/*
+        Wrap IconButton in span so it can receive mouse events
+        (e.g. show the tooltip) even if the IconButton is disabled
+      */}
+      <span>
+        <IconButton {...iconButtonProps}>{icon}</IconButton>
+      </span>
+    </Tooltip>
+  );
+}
+
+MiradorMenuButton.propTypes = {
+  'aria-label': PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
+};
