4e4da7cdc38761218f52157c883a9f046b2922b2
Glenn Fischer <gfischer@ub.uni-leipzig.de>
2019-03-12 10:06:40 +0100

    #1874: adds test, fixes errorwhen windows to be imported are less than already existing ones

--- src/state/selectors/index.js
+++ src/state/selectors/index.js
@@ -274,9 +274,9 @@ export function getLanguagesFromConfigWithCurrent(state) {
 }
 
 /**
- * Returns the latest error from the state
+ * Returns the first error from the state
  * @param {object} state
  */
-export function getLatestError(state) {
+export function getFirstError(state) {
   return state.errors.items[0] && state.errors[state.errors.items[0]];
 }
