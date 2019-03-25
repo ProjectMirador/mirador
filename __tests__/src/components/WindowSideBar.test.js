375be27cd90c869899081daaa771f0547b5cf2dd
Glenn Fischer <gfischer@ub.uni-leipzig.de>
2019-03-25 15:03:41 +0100

    re-add tests

--- __tests__/src/components/WindowSideBar.test.js
+++ __tests__/src/components/WindowSideBar.test.js
@@ -1,17 +1,86 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import Drawer from '@material-ui/core/Drawer';
-import WindowSideBarButtons from '../../../src/containers/WindowSideBarButtons';
-import { WindowSideBar } from '../../../src/components/WindowSideBar';
+import { mount } from 'enzyme';
+import { WindowSideBarButtons } from '../../../src/components/WindowSideBarButtons';
 
-describe('WindowSideBar', () => {
+/** create wrapper */
+function createWrapper(props) {
+  return mount(
+    <WindowSideBarButtons
+      addCompanionWindow={() => {}}
+      {...props}
+    />,
+  );
+}
+
+/**
+ * Assert which tab is focussed
+ */
+function assertTabFocused(wrapper, tabIndex) {
+  const tabs = wrapper.find('button[role="tab"]');
+
+  tabs.forEach((tab, index) => {
+    if (index === tabIndex) {
+      expect(tab.find('button').instance()).toEqual(document.activeElement);
+    }
+  });
+}
+
+describe('WindowSideBarButtons (shallow)', () => {
+  const windowId = 'test123';
   let wrapper;
+
   beforeEach(() => {
-    wrapper = shallow(<WindowSideBar windowId="1" classes={{}} />);
+    wrapper = createWrapper({ windowId });
   });
 
   it('renders without an error', () => {
-    expect(wrapper.find(Drawer).length).toBe(1);
-    expect(wrapper.find(WindowSideBarButtons).length).toBe(1);
+    expect(wrapper.find('WithStyles(Tabs)').length).toBe(1);
+  });
+
+  it('triggers the addCompanionWindow prop on click', () => {
+    const addCompanionWindow = jest.fn();
+    wrapper = createWrapper({ addCompanionWindow, windowId });
+
+    wrapper.find('WithStyles(Tabs)').props().onChange({ target: { removeAttribute: () => {}, setAttribute: () => {} } }, 'info');
+    expect(addCompanionWindow).toHaveBeenCalledTimes(1);
+    expect(addCompanionWindow).toHaveBeenCalledWith('info');
+  });
+
+  it('has a badge indicating if the annotations panel has annotations', () => {
+    let tab;
+    wrapper = createWrapper({ hasAnnotations: true, windowId });
+    tab = wrapper.find('WithStyles(Tab)[aria-label="openAnnotationCompanionWindow"]');
+    expect(tab.find('WithStyles(Badge)').props().invisible).toBe(false);
+
+    wrapper = createWrapper({ hasAnnotations: false, windowId });
+    tab = wrapper.find('WithStyles(Tab)[aria-label="openAnnotationCompanionWindow"]');
+
+    expect(tab.find('WithStyles(Badge)').props().invisible).toBe(true);
+  });
+
+  describe('handleKeyUp', () => {
+    it('the first tab is focussed by default', () => {
+      assertTabFocused(wrapper, 0);
+    });
+
+    it('the focuses on the next tab when pressing the down arrow', () => {
+      wrapper.find('button[role="tab"]').at(0).simulate('keyUp', { key: 'ArrowDown' });
+      assertTabFocused(wrapper, 1);
+    });
+
+    it('the focuses on the previous tab when pressing the up arrow', () => {
+      wrapper.find('button[role="tab"]').at(1).simulate('keyUp', { key: 'ArrowUp' });
+      assertTabFocused(wrapper, 0);
+    });
+
+    it('the focuses on the first tab when pressing the down arrow from the last tab', () => {
+      wrapper.find('button[role="tab"]').last().simulate('keyUp', { key: 'ArrowDown' });
+      assertTabFocused(wrapper, 0);
+    });
+
+    it('the focuses on the last tab when pressing the up arrow from the first tab', () => {
+      wrapper.find('button[role="tab"]').first().simulate('keyUp', { key: 'ArrowUp' });
+      assertTabFocused(wrapper, 2); // Assuming 3 tabs
+    });
   });
 });
