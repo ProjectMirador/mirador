e924c3a601a7b618856a68f8531402f19a8da1c4
Glenn Fischer <46319140+glefi@users.noreply.github.com>
2019-03-25 16:17:31 +0100

    2034 optimize sidebar structure for keyboard support (#2165)
    * #2043: add keyboard support to WindowSidebarButtons
    
    * #2034: add keyboard support to WindowsSideBarButtons
    
    * #2034: adds focus handling
    
    * refactors the arrow key navigation
    
    * cosmetics
    
    * #2034: fix build
    
    * #2034 refactoring
    
    * #2034: fixes tests
    
    * #2034 cosmetics
    
    * add missing semicolon
    
    * cosmetics
    
    * remove console.debug()
    
    * Update the WindowSideBar focus implementation to use refs and ReactDOM.findDOMNode instead of document.querySelectorAll (which allows us to test a bit easier) (#2284)
    
    * re-add tests
    
    * fix build

--- __tests__/src/components/WindowSideBarButtons.test.js
+++ __tests__/src/components/WindowSideBarButtons.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { WindowSideBarButtons } from '../../../src/components/WindowSideBarButtons';
 
 /** create wrapper */
 function createWrapper(props) {
-  return shallow(
+  return mount(
     <WindowSideBarButtons
       addCompanionWindow={() => {}}
       {...props}
@@ -12,10 +12,25 @@ function createWrapper(props) {
   );
 }
 
-describe('WindowSideBarButtons', () => {
+/**
+ * Assert which tab is focussed
+ */
+function assertTabFocused(wrapper, tabIndex) {
+  const tabs = wrapper.find('button[role="tab"]');
+
+  tabs.forEach((tab, index) => {
+    if (index === tabIndex) {
+      expect(tab.find('button').instance()).toEqual(document.activeElement);
+    }
+  });
+}
+
+describe('WindowSideBarButtons (shallow)', () => {
+  const windowId = 'test123';
   let wrapper;
+
   beforeEach(() => {
-    wrapper = createWrapper();
+    wrapper = createWrapper({ windowId });
   });
 
   it('renders without an error', () => {
@@ -24,22 +39,48 @@ describe('WindowSideBarButtons', () => {
 
   it('triggers the addCompanionWindow prop on click', () => {
     const addCompanionWindow = jest.fn();
-    wrapper = createWrapper({ addCompanionWindow });
+    wrapper = createWrapper({ addCompanionWindow, windowId });
 
-    wrapper.find('WithStyles(Tabs)').simulate('change', {}, 'info');
+    wrapper.find('WithStyles(Tabs)').props().onChange({ target: { removeAttribute: () => {}, setAttribute: () => {} } }, 'info');
     expect(addCompanionWindow).toHaveBeenCalledTimes(1);
     expect(addCompanionWindow).toHaveBeenCalledWith('info');
   });
 
   it('has a badge indicating if the annotations panel has annotations', () => {
     let tab;
-    wrapper = createWrapper({ hasAnnotations: true });
-    tab = wrapper.find('WithStyles(Tab)[aria-label="openAnnotationCompanionWindow"]').dive().dive();
+    wrapper = createWrapper({ hasAnnotations: true, windowId });
+    tab = wrapper.find('WithStyles(Tab)[aria-label="openAnnotationCompanionWindow"]');
     expect(tab.find('WithStyles(Badge)').props().invisible).toBe(false);
 
-    wrapper = createWrapper({ hasAnnotations: false });
-    tab = wrapper.find('WithStyles(Tab)[aria-label="openAnnotationCompanionWindow"]').dive().dive();
+    wrapper = createWrapper({ hasAnnotations: false, windowId });
+    tab = wrapper.find('WithStyles(Tab)[aria-label="openAnnotationCompanionWindow"]');
 
     expect(tab.find('WithStyles(Badge)').props().invisible).toBe(true);
+  });
+
+  describe('handleKeyUp', () => {
+    it('the first tab is focussed by default', () => {
+      assertTabFocused(wrapper, 0);
+    });
+
+    it('the focuses on the next tab when pressing the down arrow', () => {
+      wrapper.find('button[role="tab"]').at(0).simulate('keyUp', { key: 'ArrowDown' });
+      assertTabFocused(wrapper, 1);
+    });
+
+    it('the focuses on the previous tab when pressing the up arrow', () => {
+      wrapper.find('button[role="tab"]').at(1).simulate('keyUp', { key: 'ArrowUp' });
+      assertTabFocused(wrapper, 0);
+    });
+
+    it('the focuses on the first tab when pressing the down arrow from the last tab', () => {
+      wrapper.find('button[role="tab"]').last().simulate('keyUp', { key: 'ArrowDown' });
+      assertTabFocused(wrapper, 0);
+    });
+
+    it('the focuses on the last tab when pressing the up arrow from the first tab', () => {
+      wrapper.find('button[role="tab"]').first().simulate('keyUp', { key: 'ArrowUp' });
+      assertTabFocused(wrapper, 2); // Assuming 3 tabs
+    });
   });
 });
--- src/components/WindowSideBarButtons.js
+++ src/components/WindowSideBarButtons.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Badge from '@material-ui/core/Badge';
 import Tabs from '@material-ui/core/Tabs';
@@ -12,17 +13,125 @@ import CanvasIndexIcon from './icons/CanvasIndexIcon';
  *
  */
 export class WindowSideBarButtons extends Component {
+  /**
+   * selects the given tab
+   * @param {*} tab the tab to activate
+   */
+  static activateTab(tab) {
+    tab.removeAttribute('tabindex');
+    tab.setAttribute('aria-selected', 'true');
+  }
+
   /** */
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.keys = {
+      down: 'ArrowDown',
+      end: 'End',
+      home: 'Home',
+      up: 'ArrowUp',
+    };
+
+    this.chars = {
+      down: 40,
+      end: 35,
+      home: 36,
+      up: 38,
+    };
   }
 
-  /** */
+  /**
+   *
+   */
+  componentDidMount() {
+    this.tabBar = ReactDOM.findDOMNode(this.tabsRef); // eslint-disable-line react/no-find-dom-node
+    this.tabs = Array.from(this.tabBar.querySelectorAll('button[role=tab]'));
+
+    /*
+      the change event isn't fired, when the tabs component is initialized,
+      so we have to perform the required actions on our own
+    */
+    const selectedTab = this.tabs.find(t => (t.getAttribute('aria-selected')) === 'true') || this.tabs[0];
+    this.selectTab(selectedTab);
+    selectedTab.focus();
+  }
+
+  /**
+   * Set the ref to the parent tabs element
+   */
+  setTabsRef(ref) {
+    if (this.tabsRef) return;
+
+    this.tabsRef = ref;
+  }
+
+  /**
+   * @param {object} event the change event
+   * @param {string} value the tab's value
+  */
   handleChange(event, value) {
     const { addCompanionWindow } = this.props;
-
+    const tab = event.target;
+    this.selectTab(tab);
     addCompanionWindow(value);
+  }
+
+  /**
+   *
+   * @param {*} tab the tab to select
+   */
+  selectTab(tab) {
+    WindowSideBarButtons.activateTab(tab);
+    this.deactivateTabs(this.tabs.indexOf(tab));
+  }
+
+  /**
+   * @param {number} omit tab index to omit
+   */
+  deactivateTabs(omit = -1) {
+    this.tabs.map((v, k) => {
+      if (k !== omit) {
+        v.setAttribute('tabindex', '-1');
+        v.setAttribute('aria-selected', 'false');
+      }
+      return null;
+    });
+  }
+
+  /**
+   *
+   * @param {object} event the keyUp event
+   */
+  handleKeyUp(event) {
+    if (event.key === this.keys.up || event.which === this.chars.up) {
+      event.preventDefault();
+      return this.focusPreviousTab(event.target);
+    }
+    if (event.key === this.keys.down || event.which === this.chars.down) {
+      event.preventDefault();
+      return this.focusNextTab(event.target);
+    }
+    return null;
+  }
+
+  /**
+   * focus the previous tab
+   * @param {object} tab the currently selected tab
+   */
+  focusPreviousTab(tab) {
+    const previousTab = tab.previousSibling || this.tabs[this.tabs.length - 1];
+    previousTab.focus();
+  }
+
+  /**
+   * focus the next tab
+   * @param {object} tab the currently selected tab
+   */
+  focusNextTab(tab) {
+    const nextTab = tab.nextSibling || this.tabs[0];
+    nextTab.focus();
   }
 
   /**
@@ -32,7 +141,10 @@ export class WindowSideBarButtons extends Component {
    */
   render() {
     const {
-      classes, hasAnnotations, sideBarPanel, t,
+      classes,
+      hasAnnotations,
+      sideBarPanel,
+      t,
     } = this.props;
 
     return (
@@ -44,6 +156,7 @@ export class WindowSideBarButtons extends Component {
         indicatorColor="secondary"
         textColor="secondary"
         aria-orientation="vertical"
+        ref={ref => this.setTabsRef(ref)}
       >
         <Tab
           classes={{ root: classes.tab, selected: classes.tabSelected }}
@@ -56,6 +169,7 @@ export class WindowSideBarButtons extends Component {
             </Tooltip>
           )}
           TouchRippleProps={{ classes: { child: classes.tabRipple } }}
+          onKeyUp={this.handleKeyUp}
           value="info"
         />
         <Tab
@@ -69,6 +183,7 @@ export class WindowSideBarButtons extends Component {
             </Tooltip>
           )}
           TouchRippleProps={{ classes: { child: classes.tabRipple } }}
+          onKeyUp={this.handleKeyUp}
           value="canvas_navigation"
         />
         <Tab
@@ -84,6 +199,7 @@ export class WindowSideBarButtons extends Component {
             </Tooltip>
           )}
           TouchRippleProps={{ classes: { child: classes.tabRipple } }}
+          onKeyUp={this.handleKeyUp}
           value="annotations"
         />
       </Tabs>
