2a5efce7b1547edd76909cc0316a78329ad111a1
Mathias Maaß <mathias.maass@uni-leipzig.de>
2019-03-25 15:06:42 +0100

    integration test for plugin order

---
+++ __tests__/integration/mirador/plugins/order.test.js
@@ -0,0 +1,15 @@
+
+describe('try to apply 2 add plugins and 2 wrap plugins to <WorkspaceControlPanelButtons>', () => {
+  beforeAll(async () => {
+    await page.goto('http://127.0.0.1:4488/__tests__/integration/mirador/plugins/order.html');
+    await expect(page).toMatchElement('.mirador-viewer');
+    await page.waitFor(1000);
+  });
+
+  it('only apply the first wrap plugin', async () => {
+    await expect(page).toMatchElement('#wrap-plugin-component-a .mirador-workspace-control-panel-buttons');
+    await expect(page).not.toMatchElement('#wrap-plugin-component-b');
+    await expect(page).not.toMatchElement('#add-plugin-component-a');
+    await expect(page).not.toMatchElement('#add-plugin-component-b');
+  });
+});
