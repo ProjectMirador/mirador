755a10657daaf96f6ca1ec40ac7cb91d287f1196
Mathias Maaß <mathias.maass@uni-leipzig.de>
2019-03-25 13:21:26 +0100

    intergration test for add plugins

---
+++ __tests__/integration/mirador/plugins/add.test.js
@@ -0,0 +1,13 @@
+
+describe('add two plugins to <WorkspaceControlPanelButtons>', () => {
+  beforeAll(async () => {
+    await page.goto('http://127.0.0.1:4488/__tests__/integration/mirador/plugins/add.html');
+    await expect(page).toMatchElement('.mirador-viewer');
+    await page.waitFor(1000);
+  });
+
+  it('all add plugins will be added to <WorkspaceControlPanelButtons>', async () => {
+    await expect(page).toMatchElement('.mirador-workspace-control-panel-buttons #add-plugin-component-a');
+    await expect(page).toMatchElement('.mirador-workspace-control-panel-buttons #add-plugin-component-b');
+  });
+});
