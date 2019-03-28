0ff8ed80792b16266d61c2aa9a8306d2fcc54f39
Glenn Fischer <gfischer@ub.uni-leipzig.de>
2019-03-28 16:17:27 +0100

    #2314: set focus to manifestURL input, adapt test

--- __tests__/src/components/ManifestForm.test.js
+++ __tests__/src/components/ManifestForm.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { ManifestForm } from '../../../src/components/ManifestForm';
 
 /** create wrapper */
 function createWrapper(props) {
-  return shallow(
+  return mount(
     <ManifestForm
       fetchManifest={() => {}}
       t={str => str}
@@ -13,16 +13,19 @@ function createWrapper(props) {
   );
 }
 
+
 describe('ManifestForm', () => {
   it('renders', () => {
-    const wrapper = createWrapper();
+    const wrapper = createWrapper({ addResourcesOpen: false });
     expect(wrapper.find('TextField[label="addManifestUrl"]').length).toBe(1);
+    wrapper.setProps({ addResourcesOpen: true });
+    expect(wrapper.find('TextField[label="addManifestUrl"] input').instance()).toEqual(document.activeElement);
     expect(wrapper.find('WithStyles(Button)[type="submit"]').length).toBe(1);
   });
 
   it('has a cancel button when a cancel action is provided', () => {
     const onCancel = jest.fn();
-    const wrapper = createWrapper({ onCancel });
+    const wrapper = createWrapper({ addResourcesOpen: true, onCancel });
     wrapper.setState({ formValue: 'asdf' });
 
     expect(wrapper.find('WithStyles(Button)[onClick]').length).toBe(1);
@@ -36,7 +39,7 @@ describe('ManifestForm', () => {
   it('triggers an action when the form is submitted', () => {
     const fetchManifest = jest.fn();
     const onSubmit = jest.fn();
-    const wrapper = createWrapper({ fetchManifest, onSubmit });
+    const wrapper = createWrapper({ addResourcesOpen: true, fetchManifest, onSubmit });
     wrapper.setState({ formValue: 'asdf' });
 
     wrapper.setState({ formValue: 'http://example.com/iiif' });
