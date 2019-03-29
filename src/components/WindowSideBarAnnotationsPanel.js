c47c0a813209a4a6d65c838c9b91eeffec4c4518
Jessie Keck <jessie.keck@gmail.com>
2019-03-28 17:02:59 -0700

    Make the "Showing N# annotations" text a semantic paragraph but styled as an h6

--- src/components/WindowSideBarAnnotationsPanel.js
+++ src/components/WindowSideBarAnnotationsPanel.js
@@ -65,7 +65,7 @@ export class WindowSideBarAnnotationsPanel extends Component {
       <CompanionWindow title={t('annotations')} paperClassName={ns('window-sidebar-annotation-panel')} windowId={windowId} id={id}>
         <AnnotationSettings windowId={windowId} />
         <div className={classes.section}>
-          <Typography variant="subtitle2">{t('showingNumAnnotations', { number: annotations.length })}</Typography>
+          <Typography component="p" variant="subtitle2">{t('showingNumAnnotations', { number: annotations.length })}</Typography>
         </div>
         {this.sanitizedAnnotations()}
       </CompanionWindow>
