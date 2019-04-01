37703ca90140212ceb82948efb2329873c0be405
Jack Reed <phillipjreed@gmail.com>
2019-04-01 12:26:58 -0600

    whydid

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import whyDidYouRender from '@welldone-software/why-did-you-render';
 import init from './init';
 import * as actions from './state/actions';
 import * as selectors from './state/selectors';
 
 export * from './components';
 export * from './state/reducers';
+
+whyDidYouRender(React, {
+  collapseGroups: true,
+  // include: [/.*/],
+  // notifier: ({ Component, displayName }) => {
+  //   console.warn(displayName);
+  // },
+  // onlyLogs: true,
+});
 
 const exports = {
   actions,
