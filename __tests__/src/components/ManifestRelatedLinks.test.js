a772a210291647aa0bf14ffffd5ec46c14443338
Chris Beer <cabeer@stanford.edu>
2019-04-08 10:54:54 -0700

    Display home page, renderings, and seeAlso manifest data

--- __tests__/src/components/ManifestRelatedLinks.test.js
+++ __tests__/src/components/ManifestRelatedLinks.test.js
@@ -11,22 +11,105 @@ describe('ManifestRelatedLinks', () => {
       wrapper = shallow(
         <ManifestRelatedLinks
           id="xyz"
+          homepage={[
+            {
+              label: 'Home page',
+              value: 'http://example.com/',
+            },
+          ]}
           manifestUrl="http://example.com/"
+          renderings={[
+            {
+              label: 'PDF Version',
+              value: 'http://example.com/pdf',
+            },
+          ]}
+          seeAlso={[
+            {
+              format: 'text/html',
+              label: 'A',
+              value: 'http://example.com/a',
+            },
+            {
+              label: null,
+              value: 'http://example.com/b',
+            },
+          ]}
           t={str => str}
         />,
       );
     });
 
-    it('renders manifest links', () => {
+    it('renders manifest homepage information', () => {
       expect(
         wrapper.find(Typography).at(2)
+          .matchesElement(
+            <Typography component="dt">iiif_homepage</Typography>,
+          ),
+      ).toBe(true);
+
+      expect(
+        wrapper.find(Typography).at(3)
+          .matchesElement(
+            <Typography component="dd"><a href="http://example.com/">Home page</a></Typography>,
+          ),
+      ).toBe(true);
+    });
+
+    it('renders manifest renderings information', () => {
+      expect(
+        wrapper.find(Typography).at(4)
+          .matchesElement(
+            <Typography component="dt">iiif_renderings</Typography>,
+          ),
+      ).toBe(true);
+
+      expect(
+        wrapper.find(Typography).at(5)
+          .matchesElement(
+            <Typography component="dd">
+              <a href="http://example.com/pdf">PDF Version</a>
+            </Typography>,
+          ),
+      ).toBe(true);
+    });
+
+    it('renders manifest seeAlso information', () => {
+      expect(
+        wrapper.find(Typography).at(6)
+          .matchesElement(
+            <Typography component="dt">iiif_seeAlso</Typography>,
+          ),
+      ).toBe(true);
+
+      expect(
+        wrapper.find(Typography).at(7)
+          .matchesElement(
+            <Typography component="dd">
+              <a href="http://example.com/a">A</a>
+              <Typography>(text/html)</Typography>
+            </Typography>,
+          ),
+      ).toBe(true);
+
+      expect(
+        wrapper.find(Typography).at(9)
+          .matchesElement(
+            <Typography component="dd"><a href="http://example.com/b">http://example.com/b</a></Typography>,
+          ),
+      ).toBe(true);
+    });
+
+    it('renders manifest links', () => {
+      expect(
+        wrapper.find(Typography).at(10)
           .matchesElement(
             <Typography component="dt">iiif_manifest</Typography>,
           ),
       ).toBe(true);
 
       expect(
-        wrapper.find(Typography).at(3)
+        wrapper.find(Typography).at(11)
           .matchesElement(
             <Typography component="dd"><a href="http://example.com/">http://example.com/</a></Typography>,
           ),
--- src/components/ManifestRelatedLinks.js
+++ src/components/ManifestRelatedLinks.js
@@ -14,7 +14,10 @@ export class ManifestRelatedLinks extends Component {
    */
   render() {
     const {
+      homepage,
       manifestUrl,
+      renderings,
+      seeAlso,
       id,
       t,
     } = this.props;
@@ -26,6 +29,45 @@ export class ManifestRelatedLinks extends Component {
           {t('links')}
         </Typography>
         <dl className={ns('label-value-metadata')}>
+          { homepage && (
+            <>
+              <Typography variant="subtitle2" component="dt">{t('iiif_homepage')}</Typography>
+              {
+                homepage.map(page => (
+                  <Typography key={page.value} variant="body1" component="dd">
+                    <a target="_blank" rel="noopener noreferrer" href={page.value}>{page.label || page.value}</a>
+                  </Typography>
+                ))
+              }
+            </>
+          )}
+          { renderings && renderings.length > 0 && (
+            <>
+              <Typography variant="subtitle2" component="dt">{t('iiif_renderings')}</Typography>
+              {
+                renderings.map(rendering => (
+                  <Typography key={rendering.value} variant="body1" component="dd">
+                    <a target="_blank" rel="noopener noreferrer" href={rendering.value}>{rendering.label || rendering.value}</a>
+                  </Typography>
+                ))
+              }
+            </>
+          )}
+          { seeAlso && (
+            <>
+              <Typography variant="subtitle2" component="dt">{t('iiif_seeAlso')}</Typography>
+              {
+                seeAlso.map(related => (
+                  <Typography key={related.value} variant="body1" component="dd">
+                    <a target="_blank" rel="noopener noreferrer" href={related.value}>{related.label || related.value}</a>
+                    { related.format && (
+                      <Typography variant="body2" component="span">{`(${related.format})`}</Typography>
+                    )}
+                  </Typography>
+                ))
+              }
+            </>
+          )}
           { manifestUrl && (
             <>
               <Typography variant="subtitle2" component="dt">{t('iiif_manifest')}</Typography>
@@ -41,12 +83,28 @@ export class ManifestRelatedLinks extends Component {
 }
 
 ManifestRelatedLinks.propTypes = {
+  homepage: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string,
+    value: PropTypes.string,
+  })),
   id: PropTypes.string.isRequired,
   manifestUrl: PropTypes.string,
+  renderings: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string,
+    value: PropTypes.string,
+  })),
+  seeAlso: PropTypes.arrayOf(PropTypes.shape({
+    format: PropTypes.string,
+    label: PropTypes.string,
+    value: PropTypes.string,
+  })),
   t: PropTypes.func,
 };
 
 ManifestRelatedLinks.defaultProps = {
+  homepage: null,
   manifestUrl: null,
+  renderings: null,
+  seeAlso: null,
   t: key => key,
 };
