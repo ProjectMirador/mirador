b268836ec81c2fedf9abf163a3e7231ed29dcabe
Glenn Fischer <gfischer@ub.uni-leipzig.de>
2019-04-09 12:02:00 +0200

    updates error dialog

--- src/components/ErrorDialog.js
+++ src/components/ErrorDialog.js
@@ -3,7 +3,7 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import PropTypes from 'prop-types';
-import { DialogActions, DialogContentText } from '@material-ui/core';
+import { DialogActions, DialogContentText, Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import {
   first,
@@ -28,25 +28,28 @@ export class ErrorDialog extends Component {
     const error = first(values(omit(errors, 'items')));
     const hasError = !isUndefined(error);
 
-    return (
-      <div>
-        { hasError && (
-          <Dialog onClose={() => removeError(error.id)} open={hasError}>
-            <DialogTitle>{t('errorDialogTitle')}</DialogTitle>
-            <DialogContent>
-              <DialogContentText variant="body2" noWrap color="inherit">
-                {error.message}
-              </DialogContentText>
-              <DialogActions>
-                <Button onClick={() => removeError(error.id)}>
-                  {t('errorDialogConfirm')}
-                </Button>
-              </DialogActions>
-            </DialogContent>
-          </Dialog>
-        )}
-      </div>
-    );
+    return error ? (
+      <Dialog
+        aria-labelledby="error-dialog-title"
+        id="error-dialog"
+        onClose={() => removeError(error.id)}
+        open={hasError}
+      >
+        <DialogTitle id="error-dialog-title" disableTypography>
+          <Typography variant="h2">{t('errorDialogTitle')}</Typography>
+        </DialogTitle>
+        <DialogContent disableTypography>
+          <DialogContentText variant="body2" noWrap color="inherit">
+            {error.message}
+          </DialogContentText>
+          <DialogActions>
+            <Button onClick={() => removeError(error.id)} variant="contained">
+              {t('errorDialogConfirm')}
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+    ) : null;
   }
 }
 
