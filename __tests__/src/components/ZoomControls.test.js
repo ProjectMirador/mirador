53be49aa4f8791d80fe1ea87b9cf81bd08d33c9f
Jessie Keck <jessie.keck@gmail.com>
2019-04-09 13:19:30 -0700

    Refactor ZoomControls test to have a createWrapper function.

--- __tests__/src/components/ZoomControls.test.js
+++ __tests__/src/components/ZoomControls.test.js
@@ -3,6 +3,19 @@ import { shallow } from 'enzyme';
 import MiradorMenuButton from '../../../src/containers/MiradorMenuButton';
 import { ZoomControls } from '../../../src/components/ZoomControls';
 
+/** Utility function to create a shallow rendering */
+function createWrapper(props) {
+  return shallow(
+    <ZoomControls
+      classes={{ divider: 'divider', zoom_controls: 'zoom_controls' }}
+      windowId="xyz"
+      zoomToWorld={() => {}}
+      {...props}
+
+    />,
+  );
+}
+
 describe('ZoomControls', () => {
   let wrapper;
   const viewer = { x: 100, y: 100, zoom: 1 };
@@ -11,16 +24,7 @@ describe('ZoomControls', () => {
 
   beforeEach(() => {
     updateViewport = jest.fn();
-    wrapper = shallow(
-      <ZoomControls
-        classes={{ zoom_controls: 'zoom_controls' }}
-        windowId="xyz"
-        viewer={viewer}
-        showZoomControls={showZoomControls}
-        updateViewport={updateViewport}
-        zoomToWorld={() => {}}
-      />,
-    );
+    wrapper = createWrapper({ showZoomControls, updateViewport, viewer });
   });
 
   describe('with showZoomControls=false', () => {
@@ -29,21 +33,13 @@ describe('ZoomControls', () => {
     });
   });
 
-
   describe('with showZoomControls=true', () => {
     const zoomToWorld = jest.fn();
     beforeEach(() => {
       updateViewport = jest.fn();
-      wrapper = shallow(
-        <ZoomControls
-          classes={{ zoom_controls: 'zoom_controls' }}
-          windowId="xyz"
-          viewer={viewer}
-          showZoomControls
-          updateViewport={updateViewport}
-          zoomToWorld={zoomToWorld}
-        />,
-      );
+      wrapper = createWrapper({
+        showZoomControls: true, updateViewport, viewer, zoomToWorld,
+      });
     });
 
     it('renders a couple buttons', () => {
