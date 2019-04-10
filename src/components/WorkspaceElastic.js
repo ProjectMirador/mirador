594d56ca529df8cc2b001eeeb46fe4856779aa78
Chris Beer <cabeer@stanford.edu>
2019-04-10 15:39:49 -0700

    Extract WorkspaceElasticWindow component

--- src/components/WorkspaceElastic.js
+++ src/components/WorkspaceElastic.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Rnd } from 'react-rnd';
 import ResizeObserver from 'react-resize-observer';
+import WorkspaceElasticWindow from './WorkspaceElasticWindow';
 import Window from '../containers/Window';
 import ns from '../config/css-ns';
 
@@ -59,34 +60,11 @@ class WorkspaceElastic extends React.Component {
         >
           {
             Object.values(elasticLayout).map(layout => (
-              <Rnd
-                key={`${layout.windowId}-${workspace.id}`}
-                size={{ height: layout.height, width: layout.width }}
-                position={{ x: layout.x + offsetX, y: layout.y + offsetY }}
-                bounds="parent"
-                onDragStop={(e, d) => {
-                  updateElasticWindowLayout(
-                    layout.windowId,
-                    { x: d.x - offsetX, y: d.y - offsetY },
-                  );
-                }}
-                onResize={(e, direction, ref, delta, position) => {
-                  updateElasticWindowLayout(layout.windowId, {
-                    height: ref.style.height,
-                    width: ref.style.width,
-                    x: position.x - offsetX,
-                    y: position.y - offsetY,
-                  });
-                }}
-                dragHandleClassName={ns('window-top-bar')}
-                className={
-                  workspace.focusedWindowId === layout.windowId ? ns('workspace-focused-window') : null
-                }
-              >
-                <Window
-                  window={window}
-                />
-              </Rnd>
+              <WorkspaceElasticWindow
+                layout={layout}
+                workspace={workspace}
+                updateElasticWindowLayout={updateElasticWindowLayout}
+              />
             ))
           }
         </Rnd>
