4381bc2a560cb59da124247466841e078b6639dd
Chris Beer <cabeer@stanford.edu>
2019-04-10 11:28:53 -0700

    Remove unused import

--- src/components/MiradorMenuButton.js
+++ src/components/MiradorMenuButton.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
