800e37ee623386c624d2169cd04a1f1fb0cd21d6
Jack Reed <phillipjreed@gmail.com>
2019-04-11 10:39:24 -0600

    Remove unused index.js file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import init from './init';
 import * as actions from './state/actions';
 import * as selectors from './state/selectors';
 
-export * from './components';
 export * from './state/reducers';
 
 const exports = {
