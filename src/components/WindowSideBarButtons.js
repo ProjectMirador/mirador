4faf7c45cb3230fb9906a5beba6bd3f78f388d05
Jack Reed <phillipjreed@gmail.com>
2019-04-15 17:20:16 -0600

    update badge color secondary

--- src/components/WindowSideBarButtons.js
+++ src/components/WindowSideBarButtons.js
@@ -179,7 +179,7 @@ export class WindowSideBarButtons extends Component {
           <TabButton
             value="annotations"
             icon={(
-              <Badge color="error" invisible={!hasAnnotations} variant="dot">
+              <Badge color="secondary" invisible={!hasAnnotations} variant="dot">
                 <AnnotationIcon />
               </Badge>
             )}
