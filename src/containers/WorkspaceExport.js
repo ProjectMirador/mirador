2f6597c1eeef65798893d43f253394a6b16543fd
Camille Villa <cvilla100@gmail.com>
2019-04-17 17:34:03 -0700

    Use theme colors for styling export dialog

--- src/containers/WorkspaceExport.js
+++ src/containers/WorkspaceExport.js
@@ -1,6 +1,7 @@
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withTranslation } from 'react-i18next';
+import { withStyles } from '@material-ui/core';
 import { withPlugins } from '../extend/withPlugins';
 import { WorkspaceExport } from '../components/WorkspaceExport';
 
@@ -11,8 +12,20 @@ import { WorkspaceExport } from '../components/WorkspaceExport';
  */
 const mapStateToProps = state => ({ state });
 
+/**
+ *
+ * @param theme
+ */
+const styles = theme => ({
+  dialogcontent: {
+    backgroundColor: theme.palette.shades.light,
+    color: theme.palette.text.primary,
+  },
+});
+
 const enhance = compose(
   withTranslation(),
+  withStyles(styles),
   connect(mapStateToProps, {}),
   withPlugins('WorkspaceExport'),
 );
