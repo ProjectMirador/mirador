5eb9e5dd5f683caec95c0305f668dbb6642e596d
Mathias Maaß <mathias.maass@uni-leipzig.de>
2019-04-23 17:26:31 +0200

    Create plugin list item component. Part of #2395.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import init from './init';
 import * as actions from './state/actions';
 import * as selectors from './state/selectors';
+import PluginListItem from './components/PluginListItem';
 
 export * from './state/reducers';
 
 const exports = {
   actions,
+  components: {
+    PluginListItem,
+  },
   selectors,
   viewer: init,
 };
