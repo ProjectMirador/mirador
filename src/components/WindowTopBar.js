36622aa458eefef875d37984134c6d86b964c8bc
Jack Reed <phillipjreed@gmail.com>
2019-05-30 11:10:34 -0600

    Set AppBar color to default

--- src/components/WindowTopBar.js
+++ src/components/WindowTopBar.js
@@ -31,7 +31,7 @@ export class WindowTopBar extends Component {
     } = this.props;
 
     return (
-      <AppBar position="relative">
+      <AppBar position="relative" color="default">
         <nav aria-label={t('windowNavigation')}>
           <Toolbar
             disableGutters
