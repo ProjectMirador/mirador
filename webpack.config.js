61870e696bc821f016fbc030699e106112cbd6df
Jack Reed <phillipjreed@gmail.com>
2019-07-01 08:25:08 +0300

    Update build to remove unused style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -35,14 +35,6 @@ const baseConfig = [
     module: {
       rules: [
         babelLoaderConfig,
-        {
-          test: /\.s?css$/,
-          use: [
-            'style-loader', // creates style nodes from JS strings
-            'css-loader', // translates CSS into CommonJS
-            'sass-loader', // compiles Sass to CSS, using Node Sass by default
-          ],
-        },
       ],
     },
     optimization: {
