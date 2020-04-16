1e96bb0bacebacfc9712f8d35decae110b974ecd
Jack Reed <phillipjreed@gmail.com>
2020-04-16 15:17:51 -0600

    Replace react-iframe-comm for icomcom-react fixes #2863

--- __tests__/src/components/AccessTokenSender.test.js
+++ __tests__/src/components/AccessTokenSender.test.js
@@ -29,14 +29,14 @@ describe('AccessTokenSender', () => {
       writable: true,
     });
     wrapper = createWrapper({ url: 'http://example.com' });
-    expect(wrapper.find('IframeComm').length).toBe(1);
-    expect(wrapper.find('IframeComm').props().attributes.src).toBe('http://example.com?origin=http://localhost&messageId=http://example.com');
+    expect(wrapper.find('IComCom').length).toBe(1);
+    expect(wrapper.find('IComCom').props().attributes.src).toBe('http://example.com?origin=http://localhost&messageId=http://example.com');
   });
 
   it('triggers an action when the iframe sends a message', () => {
     const handleAccessTokenMessage = jest.fn();
     wrapper = createWrapper({ handleAccessTokenMessage, url: 'http://example.com' });
-    expect(wrapper.find('IframeComm').props().handleReceiveMessage).toEqual(wrapper.instance().onReceiveAccessTokenMessage);
+    expect(wrapper.find('IComCom').props().handleReceiveMessage).toEqual(wrapper.instance().onReceiveAccessTokenMessage);
 
     wrapper.instance().onReceiveAccessTokenMessage({ data: { messageId: 'http://example.com' } });
     expect(handleAccessTokenMessage).toHaveBeenCalledWith({ messageId: 'http://example.com' });
