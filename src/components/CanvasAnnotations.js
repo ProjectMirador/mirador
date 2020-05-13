3253eb1a322632654b5276f63c0614a8a24ebf55
Jack Reed <phillipjreed@gmail.com>
2020-05-13 16:54:37 -0600

    Enable configurable HTML sanitization ruleset for annotation display fixes #2871

--- src/components/CanvasAnnotations.js
+++ src/components/CanvasAnnotations.js
@@ -58,7 +58,7 @@ export class CanvasAnnotations extends Component {
   render() {
     const {
       annotations, classes, index, label, selectedAnnotationIds, t, totalSize,
-      listContainerComponent,
+      listContainerComponent, htmlSanitizationRuleSet,
     } = this.props;
     if (annotations.length === 0) return <></>;
 
@@ -84,7 +84,10 @@ export class CanvasAnnotations extends Component {
                 onMouseLeave={this.handleAnnotationUnHighlight}
               >
                 <ListItemText primaryTypographyProps={{ variant: 'body2' }}>
-                  <SanitizedHtml ruleSet="iiif" htmlString={annotation.content} />
+                  <SanitizedHtml
+                    ruleSet={htmlSanitizationRuleSet}
+                    htmlString={annotation.content}
+                  />
                 </ListItemText>
               </ListItem>
             ))
@@ -106,6 +109,7 @@ CanvasAnnotations.propTypes = {
   classes: PropTypes.objectOf(PropTypes.string),
   deselectAnnotation: PropTypes.func.isRequired,
   highlightAnnotation: PropTypes.func.isRequired,
+  htmlSanitizationRuleSet: PropTypes.string,
   index: PropTypes.number.isRequired,
   label: PropTypes.string.isRequired,
   listContainerComponent: PropTypes.elementType,
@@ -118,6 +122,7 @@ CanvasAnnotations.propTypes = {
 CanvasAnnotations.defaultProps = {
   annotations: [],
   classes: {},
+  htmlSanitizationRuleSet: 'iiif',
   listContainerComponent: 'li',
   selectedAnnotationIds: [],
 };
