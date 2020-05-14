94727c6f4659d41514317f89ef9f19c29172dc6b
Chris Beer <cabeer@stanford.edu>
2020-05-14 09:50:25 -0700

    Support facing-pages and non-paged viewing hints

--- __tests__/src/lib/CanvasGroupings.test.js
+++ __tests__/src/lib/CanvasGroupings.test.js
@@ -34,6 +34,37 @@ describe('CanvasGroupings', () => {
       it('second grouping has 2 canvases', () => {
         expect(subject.groupings()[1]).toEqual([1, 2]);
       });
+      describe('with facing pages', () => {
+        beforeEach(() => {
+          subject = new CanvasGroupings([0, 1, 2, { getViewingHint: () => 'facing-pages', id: 3 }, 4], 'book');
+        });
+        it('puts the facing page in its own grouping', () => {
+          expect(subject.groupings()[0]).toEqual([0]);
+          expect(subject.groupings()[1]).toEqual([1, 2]);
+          expect(subject.groupings()[2][0].id).toEqual(3);
+          expect(subject.groupings()[3]).toEqual([4]);
+        });
+      });
+      describe('with a non-paged canvas', () => {
+        beforeEach(() => {
+          subject = new CanvasGroupings([0, 1, { getViewingHint: () => 'non-paged', id: 'non-paged' }, 2], 'book');
+        });
+        it('puts the non-paged canvases at the end', () => {
+          expect(subject.groupings()[0]).toEqual([0]);
+          expect(subject.groupings()[1]).toEqual([1, 2]);
+          expect(subject.groupings()[2][0].id).toEqual('non-paged');
+        });
+      });
+      describe('with a non-paged canvas first', () => {
+        beforeEach(() => {
+          subject = new CanvasGroupings([{ getViewingHint: () => 'non-paged', id: 'non-paged' }, 0, 1, 2], 'book');
+        });
+        it('puts the non-paged canvases at the end and makes the first paged canvas stand alone', () => {
+          expect(subject.groupings()[0]).toEqual([0]);
+          expect(subject.groupings()[1]).toEqual([1, 2]);
+          expect(subject.groupings()[2][0].id).toEqual('non-paged');
+        });
+      });
     });
     describe('scroll', () => {
       let subject;
@@ -58,8 +89,20 @@ describe('CanvasGroupings', () => {
       beforeEach(() => {
         subject = new CanvasGroupings([0, 1, 2, 3], 'book');
       });
-      it('selects by index / 2', () => {
+      it('selects by index', () => {
         expect(subject.getCanvases(2)).toEqual([1, 2]);
+      });
+    });
+    describe('book with facing pages', () => {
+      let subject;
+      beforeEach(() => {
+        subject = new CanvasGroupings([0, 1, { getViewingHint: () => 'facing-pages', id: 2 }, 3], 'book');
+      });
+      it('selects by index', () => {
+        expect(subject.getCanvases(0)).toEqual([0]);
+        expect(subject.getCanvases(1)).toEqual([1]);
+        expect(subject.getCanvases(2)[0].id).toEqual(2);
+        expect(subject.getCanvases(3)).toEqual([3]);
       });
     });
     describe('gallery', () => {
