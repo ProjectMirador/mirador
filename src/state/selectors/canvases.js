b64c5b7219a264facb5ddd3e6d148989ba1efc15
Chris Beer <cabeer@stanford.edu>
2020-05-25 14:30:31 -0700

    Add new getVisibleCanvasIds selector and use it where appropriate

--- src/state/selectors/canvases.js
+++ src/state/selectors/canvases.js
@@ -48,6 +48,12 @@ export const getCurrentCanvas = createSelector(
 );
 
 /** */
+export const getVisibleCanvasIds = createSelector(
+  [getVisibleCanvases],
+  grouping => (grouping || []).map(canvas => canvas.id),
+);
+
+/** */
 export function getVisibleCanvases(state, args) {
   const canvas = getCurrentCanvas(state, { ...args });
   if (!canvas) return undefined;
