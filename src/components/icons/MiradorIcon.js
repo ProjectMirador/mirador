1c74cc6570aa381d508c50f87f387353d1bf397c
Chris Beer <cabeer@stanford.edu>
2020-06-20 07:15:20 -0700

    Remove double svg wrapping from the mirador icon

--- src/components/icons/MiradorIcon.js
+++ src/components/icons/MiradorIcon.js
@@ -7,11 +7,9 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 export default function MiradorIcon(props) {
   return (
     <SvgIcon viewBox="0 0 60 55" {...props}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="60" height="55" viewBox="0 0 60 55">
-        <rect width="18" height="55" />
-        <rect width="18" height="55" transform="translate(42)" />
-        <rect width="18" height="34" transform="translate(21)" />
-      </svg>
+      <rect width="18" height="55" />
+      <rect width="18" height="55" transform="translate(42)" />
+      <rect width="18" height="34" transform="translate(21)" />
     </SvgIcon>
   );
 }
