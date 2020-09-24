c0da90e7d8bc0f9dd93cdb12e4b0df6bc1d694af
Jack Reed <phillipjreed@gmail.com>
2020-09-24 07:51:07 -0600

    Basic support for subtitles/captions

--- src/components/VideoViewer.js
+++ src/components/VideoViewer.js
@@ -6,14 +6,18 @@ export class VideoViewer extends Component {
   /* eslint-disable jsx-a11y/media-has-caption */
   /** */
   render() {
-    const { classes, videoResources } = this.props;
-
+    const { captions, classes, videoResources } = this.props;
     return (
       <div className={classes.container}>
         <video controls className={classes.video}>
           {videoResources.map(video => (
             <Fragment key={video.id}>
               <source src={video.id} type={video.getFormat()} />
+            </Fragment>
+          ))}
+          {captions.map(caption => (
+            <Fragment key={caption.id}>
+              <track src={caption.id} label={caption.getLabel()} srcLang={caption.getProperty('language')} />
             </Fragment>
           ))}
         </video>
@@ -24,10 +28,12 @@ export class VideoViewer extends Component {
 }
 
 VideoViewer.propTypes = {
+  captions: PropTypes.arrayOf(PropTypes.object),
   classes: PropTypes.objectOf(PropTypes.string).isRequired,
   videoResources: PropTypes.arrayOf(PropTypes.object),
 };
 
 VideoViewer.defaultProps = {
+  captions: [],
   videoResources: [],
 };
