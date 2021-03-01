1e6fc8fb2c6d0c8e3ba3f8b883bb2d4ed91387c7
Jasper Funk-Smit <jasper.funk-smit@mfn.berlin>
2021-03-01 08:40:53 +0100

    added default tileFormat jpg to IIIFThumbnail

--- src/components/IIIFThumbnail.js
+++ src/components/IIIFThumbnail.js
@@ -203,7 +203,7 @@ IIIFThumbnail.defaultProps = {
   labelled: false,
   maxHeight: null,
   maxWidth: null,
-  tileFormat: undefined,
+  tileFormat: "jpg",
   style: {},
   thumbnail: null,
   variant: null,
