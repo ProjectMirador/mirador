631800901fe47da1b04dece47cb8b1c793b0fc58
Jasper Funk-Smit <jasper.funk-smit@mfn.berlin>
2021-06-10 15:29:02 +0200

    and removed dist again

--- dist/es/src/containers/Branding.js
+++
@@ -1,3 +0,0 @@
-import { withPlugins } from '../extend/withPlugins';
-import { Branding } from '../components/Branding';
-export default withPlugins('Branding')(Branding);
--- dist/es/src/state/selectors/utils.js
+++
@@ -1,7 +0,0 @@
-import settings from '../../config/settings';
-/** */
-
-export function miradorSlice(state) {
-  if (settings.state.slice) return state[settings.state.slice];
-  return state;
-}
