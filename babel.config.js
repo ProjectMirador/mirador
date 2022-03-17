c79d87885940c8c68447543ad6fb8b658a848dcc
Chris Beer <chris@cbeer.info>
2022-03-17 09:21:59 -0700

    Silence a webpack warning

--- babel.config.js
+++ babel.config.js
@@ -53,6 +53,8 @@ module.exports = function (api) {
         loose: true,
       },
     ],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
     [
       '@babel/plugin-proposal-object-rest-spread',
       {
