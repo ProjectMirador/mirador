c15d3156dcfb596f7bf1c166c92b26a8523ccab6
Loïs Poujade <lois.poujade@tetras-libre.fr>
2022-11-30 10:22:50 +0100

    Fix VideoViewer component tests
    - use Manifesto Utils.parseManifest to feed data to component
    - add integrations tests for captions
    - remove component captions tests (cf #13)
    - extend expect-puppeteer timeout to 2s

--- __tests__/src/components/VideoViewer.test.js
+++ __tests__/src/components/VideoViewer.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Utils } from 'manifesto.js';
 import { VideoViewer } from '../../../src/components/VideoViewer';
+import videoSimple from '../../fixtures/version-3/video.json';
 
 /** create wrapper */
 function createWrapper(props, suspenseFallback) {
@@ -16,36 +18,18 @@ function createWrapper(props, suspenseFallback) {
 describe('VideoViewer', () => {
   let wrapper;
   describe('render', () => {
+    const canvasSimple = Utils.parseManifest(videoSimple).getSequences()[0].getCanvases()[0];
     it('videoResources', () => {
       wrapper = createWrapper({
-        videoResources: [
-          { getFormat: () => 'video/mp4', id: 1 },
-          { getFormat: () => 'video/mp4', id: 2 },
-        ],
+        canvas: canvasSimple,
       }, true);
-      expect(wrapper.contains(<source src={1} type="video/mp4" />)).toBe(true);
-      expect(wrapper.contains(<source src={2} type="video/mp4" />)).toBe(true);
+      expect(wrapper.contains(<source src="https://fixtures.iiif.io/video/indiana/30-minute-clock/medium/30-minute-clock.mp4" type="video/mp4" />)).toBe(true);
     });
     it('passes through configurable options', () => {
       wrapper = createWrapper({
-        videoResources: [
-          { getFormat: () => 'video/mp4', id: 1 },
-        ],
+        canvas: canvasSimple,
       }, true);
       expect(wrapper.exists('video[crossOrigin="anonymous"]')).toBe(true); // eslint-disable-line jsx-a11y/media-has-caption
-    });
-    it('captions', () => {
-      wrapper = createWrapper({
-        captions: [
-          { getDefaultLabel: () => 'English', getProperty: () => 'en', id: 1 },
-          { getDefaultLabel: () => 'French', getProperty: () => 'fr', id: 2 },
-        ],
-        videoResources: [
-          { getFormat: () => 'video/mp4', id: 1 },
-        ],
-      }, true);
-      expect(wrapper.contains(<track src={1} label="English" srcLang="en" />)).toBe(true);
-      expect(wrapper.contains(<track src={2} label="French" srcLang="fr" />)).toBe(true);
     });
   });
 });
