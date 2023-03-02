bf89fab253ce24255d3570e2fbb07e562197e2a8
Phil Plencner <philip_plencner@harvard.edu>
2023-03-02 11:53:51 -0500

    All tests pass for WindowViewSettings.

--- __tests__/src/components/WindowViewSettings.test.js
+++ __tests__/src/components/WindowViewSettings.test.js
@@ -1,12 +1,10 @@
-import { mount } from 'enzyme';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import MenuItem from '@material-ui/core/MenuItem';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { WindowViewSettings } from '../../../src/components/WindowViewSettings';
 
 /** create wrapper */
 function createWrapper(props) {
-  return mount(
+  return render(
     <WindowViewSettings
       classes={{}}
       windowId="xyz"
@@ -20,57 +18,48 @@ function createWrapper(props) {
 
 describe('WindowViewSettings', () => {
   it('renders all elements correctly', () => {
-    const wrapper = createWrapper();
-    expect(wrapper.find(ListSubheader).length).toBe(1);
-    const labels = wrapper.find(FormControlLabel);
-    expect(labels.length).toBe(4);
-    expect(labels.at(0).props().value).toBe('single');
-    expect(labels.at(1).props().value).toBe('book');
-    expect(labels.at(2).props().value).toBe('scroll');
-    expect(labels.at(3).props().value).toBe('gallery');
+    createWrapper();
+    expect(screen.getByRole('presentation', { selector: 'li' })).toBeInTheDocument();
+    const menuItems = screen.queryAllByRole('menuitem');
+    expect(menuItems.length).toBe(4);
+    expect(menuItems[0]).toHaveTextContent(/single/i);
+    expect(menuItems[1]).toHaveTextContent(/book/i);
+    expect(menuItems[2]).toHaveTextContent(/scroll/i);
+    expect(menuItems[3]).toHaveTextContent(/gallery/i);
   });
-
-  it('should set the correct label active (by setting the secondary color)', () => {
-    let wrapper = createWrapper({ windowViewType: 'single' });
-    expect(wrapper.find(FormControlLabel).at(0).props().control.props.color).toEqual('secondary');
-    expect(wrapper.find(FormControlLabel).at(1).props().control.props.color).not.toEqual('secondary');
-
-    wrapper = createWrapper({ windowViewType: 'book' });
-    expect(wrapper.find(FormControlLabel).at(1).props().control.props.color).toEqual('secondary');
-
-    wrapper = createWrapper({ windowViewType: 'scroll' });
-    expect(wrapper.find(FormControlLabel).at(2).props().control.props.color).toEqual('secondary');
-
-    wrapper = createWrapper({ windowViewType: 'gallery' });
-    expect(wrapper.find(FormControlLabel).at(3).props().control.props.color).toEqual('secondary');
+  it('single should set the correct label active (by setting the secondary color)', () => {
+    createWrapper({ windowViewType: 'single' });
+    expect(screen.getByRole('menuitem', { name: /single/ }).querySelector('svg')).toHaveClass('MuiSvgIcon-colorSecondary'); // eslint-disable-line testing-library/no-node-access
+    expect(screen.getByRole('menuitem', { name: /book/ }).querySelector('svg')).not.toHaveClass('MuiSvgIcon-colorSecondary'); // eslint-disable-line testing-library/no-node-access
   });
-
-  it('updates state when the view config selection changes', () => {
+  it('book should set the correct label active (by setting the secondary color)', () => {
+    createWrapper({ windowViewType: 'book' });
+    expect(screen.getByRole('menuitem', { name: /book/ }).querySelector('svg')).toHaveClass('MuiSvgIcon-colorSecondary'); // eslint-disable-line testing-library/no-node-access
+    expect(screen.getByRole('menuitem', { name: /single/ }).querySelector('svg')).not.toHaveClass('MuiSvgIcon-colorSecondary'); // eslint-disable-line testing-library/no-node-access
+  });
+  it('scroll should set the correct label active (by setting the secondary color)', () => {
+    createWrapper({ windowViewType: 'scroll' });
+    expect(screen.getByRole('menuitem', { name: /scroll/ }).querySelector('svg')).toHaveClass('MuiSvgIcon-colorSecondary'); // eslint-disable-line testing-library/no-node-access
+    expect(screen.getByRole('menuitem', { name: /single/ }).querySelector('svg')).not.toHaveClass('MuiSvgIcon-colorSecondary'); // eslint-disable-line testing-library/no-node-access
+  });
+  it('gallery should set the correct label active (by setting the secondary color)', () => {
+    createWrapper({ windowViewType: 'gallery' });
+    expect(screen.getByRole('menuitem', { name: /gallery/ }).querySelector('svg')).toHaveClass('MuiSvgIcon-colorSecondary'); // eslint-disable-line testing-library/no-node-access
+    expect(screen.getByRole('menuitem', { name: /single/ }).querySelector('svg')).not.toHaveClass('MuiSvgIcon-colorSecondary'); // eslint-disable-line testing-library/no-node-access
+  });
+  it('updates state when the view config selection changes', async () => {
     const setWindowViewType = jest.fn();
-    const wrapper = createWrapper({ setWindowViewType });
-    wrapper.find(MenuItem).at(0).simulate('click');
+    createWrapper({ setWindowViewType });
+    const user = userEvent.setup();
+    const menuItems = screen.queryAllByRole('menuitem');
+    expect(menuItems.length).toBe(4);
+    await user.click(menuItems[0]);
     expect(setWindowViewType).toHaveBeenCalledWith('xyz', 'single');
-    wrapper.find(MenuItem).at(1).simulate('click');
+    await user.click(menuItems[1]);
     expect(setWindowViewType).toHaveBeenCalledWith('xyz', 'book');
-    wrapper.find(MenuItem).at(2).simulate('click');
+    await user.click(menuItems[2]);
     expect(setWindowViewType).toHaveBeenCalledWith('xyz', 'scroll');
-    wrapper.find(MenuItem).at(3).simulate('click');
+    await user.click(menuItems[3]);
     expect(setWindowViewType).toHaveBeenCalledWith('xyz', 'gallery');
-  });
-
-  it('sets autofocus on the selected MenuItem', () => {
-    const wrapper = mount(
-      <WindowViewSettings
-        classes={{}}
-        windowId="xyz"
-        setWindowViewType={() => {}}
-        viewTypes={['single', 'book', 'scroll', 'gallery']}
-        windowViewType="book"
-      />,
-    );
-
-    expect(
-      wrapper.find(MenuItem).at(1).prop('autoFocus'),
-    ).toEqual(true);
   });
 });
