8ec072a6afa5f7d1d547b3526f5620c7f4b5ef2f
Phil Plencner <40801910+phil-plencner-hl@users.noreply.github.com>
2023-03-02 10:35:27 -0500

    Convert WindowViewer and WindowThumbnailSettings to RTL

--- __tests__/src/components/WindowViewer.test.js
+++ __tests__/src/components/WindowViewer.test.js
@@ -1,31 +1,21 @@
-import { shallow } from 'enzyme';
+import { screen } from '@testing-library/react';
 import { WindowViewer } from '../../../src/components/WindowViewer';
-import WindowCanvasNavigationControls from '../../../src/containers/WindowCanvasNavigationControls';
+import { renderWithProviders } from '../../utils/store';
 
 /** create wrapper */
-function createWrapper(props, suspenseFallback) {
-  return shallow(
+function createWrapper(props) {
+  return renderWithProviders(
     <WindowViewer
-      windowId="xyz"
       {...props}
     />,
-    { suspenseFallback },
   );
 }
 
 describe('WindowViewer', () => {
-  let wrapper;
-  describe('when lazy imorts have not loaded', () => {
-    it('renders fallback', () => {
-      wrapper = createWrapper({}, true);
-      expect(wrapper.find('div').length).toBe(1);
-    });
-  });
-  describe('when lazy imorts have loaded', () => {
-    it('renders expected components', () => {
-      wrapper = createWrapper({}, false);
-      expect(wrapper.find('lazy').props().windowId).toBe('xyz');
-      expect(wrapper.find(WindowCanvasNavigationControls).props().windowId).toBe('xyz');
+  describe('when lazy imports have loaded', () => {
+    it('renders openseadragon', async () => {
+      createWrapper({});
+      await screen.findByRole('img', { selector: 'canvas' });
     });
   });
 });
