2d431a4136890a379bec9698beb500dee695accf
Marlo Longley <mlongley@stanford.edu>
2023-03-02 17:16:22 -0500

    Refactor WorkspaceOptionsMenuButton and WorkspaceOptionsMenu and convert tests to RTL

--- __tests__/src/components/WorkspaceOptionsMenu.test.js
+++ __tests__/src/components/WorkspaceOptionsMenu.test.js
@@ -1,40 +1,76 @@
-import { shallow } from 'enzyme';
-import MenuItem from '@material-ui/core/MenuItem';
-import WorkspaceExport from '../../../src/containers/WorkspaceExport';
-import WorkspaceImport from '../../../src/containers/WorkspaceImport';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { WorkspaceOptionsMenu } from '../../../src/components/WorkspaceOptionsMenu';
+import { renderWithProviders } from '../../utils/store';
 
-/** Utility helper to create a shallow wrapper around WorkspaceOptionsButton */
-function createShallow(props) {
-  return shallow(
-    <WorkspaceOptionsMenu
-      handleClose={() => {}}
-      t={k => k}
-      {...props}
-    />,
+/** create wrapper */
+function Subject({ ...props }) {
+  return (
+    <div>
+      <WorkspaceOptionsMenu
+        handleClose={() => {}}
+        t={k => k}
+        {...props}
+      />
+      ,
+      ,
+    </div>
+  );
+}
+
+/** create anchor element */
+function createAnchor() {
+  return render(
+    <button type="button" data-testid="menu-trigger-button">Button</button>,
   );
 }
 
 describe('WorkspaceOptionsMenu', () => {
-  let wrapper;
-
-  it('toggles the relevant section with each MenuItem click', () => {
-    wrapper = createShallow();
-
-    expect(wrapper.find(MenuItem).length).toEqual(2);
-    expect(wrapper.find(WorkspaceExport).length).toEqual(0);
-    expect(wrapper.find(WorkspaceImport).length).toEqual(0);
-
-    wrapper.find(MenuItem).at(0).simulate('click');
-    expect(wrapper.find(WorkspaceExport).length).toEqual(1);
-    wrapper.find(MenuItem).at(1).simulate('click');
-    expect(wrapper.find(WorkspaceImport).length).toEqual(1);
+  let user;
+  beforeEach(() => {
+    createAnchor();
+    user = userEvent.setup();
   });
 
-  it('it passes a handleClose prop to the various components to that closes that will close the component', () => {
-    wrapper.setState({ exportWorkspace: { open: true } });
-    expect(wrapper.state().exportWorkspace.open).toBe(true);
-    wrapper.find(WorkspaceExport).props().handleClose();
-    expect(wrapper.state().exportWorkspace.open).toBe(false);
+  it('renders all needed elements when open', () => {
+    renderWithProviders(<Subject anchorEl={screen.getByTestId('menu-trigger-button')} open />);
+
+    expect(screen.getByRole('menu')).toBeInTheDocument();
+
+    const menuItems = screen.getAllByRole('menuitem');
+    expect(menuItems).toHaveLength(2);
+    expect(menuItems[0]).toHaveTextContent('downloadExportWorkspace');
+    expect(menuItems[1]).toHaveTextContent('importWorkspace');
+  });
+
+  it('does not display unless open', () => {
+    renderWithProviders(<Subject open={false} />);
+    expect(screen.queryByRole('menu')).not.toBeInTheDocument();
+  });
+
+  it('renders the export dialog when export option is clicked', async () => {
+    renderWithProviders(<Subject anchorEl={screen.getByTestId('menu-trigger-button')} open />);
+    expect(document.querySelector('#workspace-export')).not.toBeInTheDocument(); // eslint-disable-line testing-library/no-node-access
+
+    await user.click(screen.getAllByRole('menuitem')[0]);
+    expect(document.querySelector('#workspace-export')).toBeInTheDocument(); // eslint-disable-line testing-library/no-node-access
+  });
+
+  it('renders the import dialog when imporrt option is clicked', async () => {
+    renderWithProviders(<Subject anchorEl={screen.getByTestId('menu-trigger-button')} open />);
+    expect(document.querySelector('#workspace-import')).not.toBeInTheDocument(); // eslint-disable-line testing-library/no-node-access
+
+    await user.click(screen.getAllByRole('menuitem')[1]);
+    expect(document.querySelector('#workspace-import')).toBeInTheDocument(); // eslint-disable-line testing-library/no-node-access
+  });
+
+  it('fires the correct callbacks on menu close', async () => {
+    const handleClose = jest.fn();
+    renderWithProviders(<Subject anchorEl={screen.getByTestId('menu-trigger-button')} handleClose={handleClose} open />);
+
+    // click a menu item should close the menu
+    const menuItems = screen.getAllByRole('menuitem');
+    await user.click(menuItems[0]);
+    expect(handleClose).toHaveBeenCalledTimes(1);
   });
 });
