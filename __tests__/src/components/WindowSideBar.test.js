0fcccd8e3f1dfc223f9d06371f2bbcafa4aea6ea
Marlo Longley <mlongley@stanford.edu>
2023-03-09 12:49:08 -0500

    Convert WindowSideBar tests to RTL

--- __tests__/src/components/WindowSideBar.test.js
+++ __tests__/src/components/WindowSideBar.test.js
@@ -1,38 +1,47 @@
-import { shallow } from 'enzyme';
-import Drawer from '@material-ui/core/Drawer';
+import { screen } from '@testing-library/react';
 import { WindowSideBar } from '../../../src/components/WindowSideBar';
+import { renderWithProviders } from '../../utils/store';
 
 /** create wrapper */
-function createWrapper(props) {
-  return shallow(
+function createWrapper({ ...props }) {
+  return renderWithProviders(
     <WindowSideBar
-      classes={{}}
+      classes={{ drawer: 'test-drawer' }}
       t={k => k}
-      windowId="1"
+      windowId="xyz"
       {...props}
     />,
+    {
+      preloadedState: {
+        windows: {
+          xyz: {
+            companionWindowIds: [],
+            suggestedSearches: null,
+          },
+        },
+      },
+    },
   );
 }
 
-describe('WindowSideBar', () => {
+describe('WindowSideBar when closed', () => {
   it('renders without an error', () => {
-    const wrapper = createWrapper();
-    expect(wrapper.find(Drawer).length).toBe(1);
-    expect(wrapper.find(Drawer).prop('open')).toBe(false);
+    createWrapper({});
+    expect(document.querySelector('.test-drawer')).toBeInTheDocument(); // eslint-disable-line testing-library/no-node-access
+    expect(screen.queryByRole('navigation', { accessibleName: 'sidebarPanelsNavigation' })).not.toBeInTheDocument();
   });
+});
+describe('WindowSideBar when open', () => {
   it('renders in an open state', () => {
-    const wrapper = createWrapper({ sideBarOpen: true });
-    expect(wrapper.find(Drawer).length).toBe(1);
-    expect(wrapper.find(Drawer).prop('open')).toBe(true);
+    createWrapper({ sideBarOpen: true });
+    expect(screen.getByRole('navigation', { accessibleName: 'sidebarPanelsNavigation' })).toBeInTheDocument();
   });
-  it('when ltr', () => {
-    const wrapper = createWrapper();
-    expect(wrapper.find(Drawer).prop('anchor')).toBe('left');
+  it('Renders drawer ltr by default', () => {
+    createWrapper({ sideBarOpen: true });
+    expect(screen.queryByRole('navigation', { accessibleName: 'sidebarPanelsNavigation' })).toHaveClass('MuiDrawer-paperAnchorLeft'); // eslint-disable-line testing-library/no-node-access
   });
-  it('when rtl', () => {
-    const wrapper = createWrapper({
-      direction: 'rtl',
-    });
-    expect(wrapper.find(Drawer).prop('anchor')).toBe('right');
+  it('Renders drawer rtl when specified', () => {
+    createWrapper({ direction: 'rtl', sideBarOpen: true });
+    expect(screen.queryByRole('navigation', { accessibleName: 'sidebarPanelsNavigation' })).toHaveClass('MuiDrawer-paperAnchorRight'); // eslint-disable-line testing-library/no-node-access
   });
 });
