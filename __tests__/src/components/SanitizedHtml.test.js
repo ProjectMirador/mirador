4f9da4f0809ed36b05de4912e48e5777c53fe756
Chris Beer <chris@cbeer.info>
2023-03-13 09:14:14 -0700

    Update SanitizedHtml tests for RTL

--- __tests__/src/components/SanitizedHtml.test.js
+++ __tests__/src/components/SanitizedHtml.test.js
@@ -1,25 +1,31 @@
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { SanitizedHtml } from '../../../src/components/SanitizedHtml';
 
-const wrapper = shallow(
-  <SanitizedHtml
-    classes={{ root: 'root' }}
-    htmlString="<script>doBadThings()</script><b>Don't worry!</b><a>Some link</a>"
-    ruleSet="iiif"
-  />,
-);
-
 describe('SanitizedHtml', () => {
+  beforeEach(() => {
+    render(
+      <SanitizedHtml
+        data-testid="subject"
+        classes={{ root: 'root' }}
+        htmlString="<script>doBadThings()</script><b>Don't worry!</b><a>Some link</a>"
+        ruleSet="iiif"
+      />,
+    );
+  });
+
   it('should render needed elements', () => {
-    expect(wrapper.find('span').length).toBe(1);
+    expect(screen.getByTestId('subject')).toHaveProperty('tagName', 'SPAN');
   });
 
   it('should pass correct class name to root element', () => {
-    expect(wrapper.find('span').first().props().className).toBe('root mirador-third-party-html');
+    expect(screen.getByTestId('subject')).toHaveClass('root');
+    expect(screen.getByTestId('subject')).toHaveClass('mirador-third-party-html');
   });
 
   it('should pass sanitized html string to dangerouslySetInnerHTML attribute', () => {
-    expect(wrapper.find('span').first().props().dangerouslySetInnerHTML)
-      .toEqual({ __html: "<b>Don't worry!</b><a target=\"_blank\" rel=\"noopener noreferrer\">Some link</a>" });
+    expect(screen.getByTestId('subject').querySelector('script')).not.toBeInTheDocument(); // eslint-disable-line testing-library/no-node-access, testing-library/prefer-presence-queries
+    expect(screen.getByText('Don\'t worry!')).toBeInTheDocument();
+    expect(screen.getByText('Some link')).toHaveAttribute('target', '_blank');
+    expect(screen.getByText('Some link')).toHaveAttribute('rel', 'noopener noreferrer');
   });
 });
