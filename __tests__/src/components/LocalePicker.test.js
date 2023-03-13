760447a01b5879129264c53a5634175b40c2ad5e
Chris Beer <chris@cbeer.info>
2023-03-13 14:54:35 -0700

    Update LocalePicker tests to user RTL

--- __tests__/src/components/LocalePicker.test.js
+++ __tests__/src/components/LocalePicker.test.js
@@ -1,13 +1,12 @@
-import { shallow } from 'enzyme';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { LocalePicker } from '../../../src/components/LocalePicker';
 
 /**
  * Helper function to create a shallow wrapper around LanguageSettings
  */
 function createWrapper(props) {
-  return shallow(
+  return render(
     <LocalePicker
       availableLocales={[]}
       locale={undefined}
@@ -18,35 +17,42 @@ function createWrapper(props) {
 }
 
 describe('LocalePicker', () => {
-  let wrapper;
-
   it('hides the control if there are not locales to switch to', () => {
-    wrapper = createWrapper({ availableLocales: ['en'] });
+    const { container } = createWrapper({ availableLocales: ['en'] });
 
-    expect(wrapper.find(Select).length).toBe(0);
+    expect(container).toBeEmptyDOMElement(); // eslint-disable-line testing-library/no-container
   });
 
   it('renders a select with the current value', () => {
-    wrapper = createWrapper({ availableLocales: ['en', 'de'], locale: 'en' });
+    createWrapper({ availableLocales: ['en', 'de'], locale: 'en' });
 
-    expect(wrapper.find(Select).length).toBe(1);
-    expect(wrapper.find(Select).props().value).toBe('en');
+    expect(screen.getByRole('button')).toHaveTextContent('en');
   });
 
-  it('renders a select with a list item for each language passed in props', () => {
-    wrapper = createWrapper({ availableLocales: ['en', 'de'] });
+  it('renders a select with a list item for each language passed in props', async () => {
+    const user = userEvent.setup();
 
-    expect(wrapper.find(MenuItem).length).toBe(2);
+    createWrapper({ availableLocales: ['en', 'de'], locale: 'en' });
+
+    await user.click(screen.getByRole('button'));
+
+    expect(screen.getAllByRole('option')).toHaveLength(2);
+    expect(screen.getByRole('option', { name: 'en' })).toBeInTheDocument();
+    expect(screen.getByRole('option', { name: 'de' })).toBeInTheDocument();
   });
 
-  it('triggers setLocale prop when clicking a list item', () => {
+  it('triggers setLocale prop when clicking a list item', async () => {
+    const user = userEvent.setup();
     const setLocale = jest.fn();
 
-    wrapper = createWrapper({
+    createWrapper({
       availableLocales: ['en', 'de'],
+      locale: 'en',
       setLocale,
     });
-    wrapper.find(Select).simulate('change', { target: { value: 'de' } });
+
+    await user.click(screen.getByRole('button'));
+    await user.click(screen.getByRole('option', { name: 'de' }));
 
     expect(setLocale).toHaveBeenCalledTimes(1);
     expect(setLocale).toHaveBeenCalledWith('de');
