5698f8f68f178dcaf91703d7c1fb8343c3dc78bd
Chris Beer <chris@cbeer.info>
2024-01-11 09:17:56 -0800

    Label the locale picker and translate locale codes where possible.

--- src/components/LocalePicker.js
+++ src/components/LocalePicker.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
+import keyBy from 'lodash/keyBy';
 import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Typography from '@mui/material/Typography';
@@ -16,15 +18,20 @@ export class LocalePicker extends Component {
   render() {
     const {
       availableLocales,
+      languages,
       locale,
       setLocale,
+      t,
     } = this.props;
 
     if (!setLocale || availableLocales.length < 2) return null;
     const selectedLocale = availableLocales.indexOf(locale) >= 0 ? locale : availableLocales[0];
 
+    const labels = keyBy(languages, o => o.locale);
+
     return (
-      <FormControl>
+      <FormControl variant="standard">
+        <InputLabel>{t('language')}</InputLabel>
         <Select
           MenuProps={{
             anchorOrigin: {
@@ -32,6 +39,7 @@ export class LocalePicker extends Component {
               vertical: 'bottom',
             },
           }}
+          autoWidth
           displayEmpty
           value={selectedLocale}
           onChange={(e) => { setLocale(e.target.value); }}
@@ -39,7 +47,7 @@ export class LocalePicker extends Component {
         >
           {
             availableLocales.map(l => (
-              <MenuItem key={l} value={l}><Typography variant="body2">{ l }</Typography></MenuItem>
+              <MenuItem key={l} value={l}><Typography variant="body2">{ labels[l]?.label || l }</Typography></MenuItem>
             ))
           }
         </Select>
@@ -50,12 +58,21 @@ export class LocalePicker extends Component {
 
 LocalePicker.propTypes = {
   availableLocales: PropTypes.arrayOf(PropTypes.string),
+  languages: PropTypes.arrayOf(
+    PropTypes.shape({
+      current: PropTypes.bool.isRequired,
+      label: PropTypes.string.isRequired,
+      locale: PropTypes.string.isRequired,
+    }),
+  ),
   locale: PropTypes.string,
   setLocale: PropTypes.func,
+  t: PropTypes.func.isRequired,
 };
 
 LocalePicker.defaultProps = {
   availableLocales: [],
+  languages: [],
   locale: '',
   setLocale: undefined,
 };
--- src/containers/LocalePicker.js
+++ src/containers/LocalePicker.js
@@ -1,9 +1,21 @@
 import { compose } from 'redux';
+import { connect } from 'react-redux';
 import { withTranslation } from 'react-i18next';
+import { withPlugins } from '../extend/withPlugins';
+import { getLanguagesFromConfigWithCurrent } from '../state/selectors';
 import { LocalePicker } from '../components/LocalePicker';
+
+/**
+ * Map state to props for connect
+ */
+const mapStateToProps = state => ({
+  languages: getLanguagesFromConfigWithCurrent(state),
+});
 
 const enhance = compose(
   withTranslation(),
+  connect(mapStateToProps),
+  withPlugins('LocalePicker'),
 );
 
 export default enhance(LocalePicker);
