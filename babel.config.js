1215b40434bb6a92d83e949f4dcb219a82a72345
Justin Coyne <jcoyne@justincoyne.com>
2024-10-28 21:38:33 -0500

    Remove babel proposal plugins
    Modern browsers have this built in

--- babel.config.js
+++ babel.config.js
@@ -48,20 +48,6 @@ module.exports = function (api) {
     'babel-plugin-macros',
     '@babel/plugin-transform-destructuring',
     [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: true,
-      },
-    ],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    [
-      '@babel/plugin-proposal-object-rest-spread',
-      {
-        useBuiltIns: true,
-      },
-    ],
-    [
       '@babel/plugin-transform-runtime',
       {
         corejs: false,
