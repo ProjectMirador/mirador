b9e87066639de1a14f52288db84ed2fa383dceeb
Justin Coyne <jcoyne@justincoyne.com>
2024-10-29 15:41:10 -0500

    Stop using default props in BackgroundPluginArea
    defaultProps are deprecated for function components

--- src/components/BackgroundPluginArea.js
+++ src/components/BackgroundPluginArea.js
@@ -1,18 +1,13 @@
-import PropTypes from 'prop-types';
 import ns from '../config/css-ns';
 import { PluginHook } from './PluginHook';
+
+const BackgroundPluginAreaDefaultProps = {
+  PluginComponents: [],
+};
 
 /** invisible area where background plugins can add to */
 export const BackgroundPluginArea = props => (
   <div className={ns('background-plugin-area')} style={{ display: 'none' }}>
-    <PluginHook {...props} />
+    <PluginHook {...BackgroundPluginAreaDefaultProps} {...props} />
   </div>
 );
-
-BackgroundPluginArea.propTypes = {
-  PluginComponents: PropTypes.array, // eslint-disable-line react/forbid-prop-types
-};
-
-BackgroundPluginArea.defaultProps = {
-  PluginComponents: [],
-};
