cdad50f5a9d184c948761228af7ccfca7164fd7e
marlo-longley <mlongley@stanford.edu>
2024-11-12 14:39:47 -0800

    Replace webpack + babel with Vite

--- src/polyfills.js
+++
@@ -1,7 +0,0 @@
-/**
- * Polyfill file used to build Webpack UMD build. These polyfills are needed to
- * support both IE and Edge. If you are using Mirador ES imports these will not
- * get bundled up.
- */
-
-import 'core-js/stable'; // eslint-disable-line import/no-extraneous-dependencies
