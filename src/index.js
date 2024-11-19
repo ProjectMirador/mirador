8abbbe9e2f7c2d466bf2f8751ad700e4dd9d6ffb
Chris Beer <cabeer@stanford.edu>
2024-11-19 09:45:04 -0800

    Add index.js files to src directories

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import init from './init';
 import state from './state';
 
+export * from './state';
+export * from './components';
+export * from './containers';
+export * from './contexts';
+export * from './extend';
+export * from './lib';
+export * from './plugins';
+
 export default {
   ...init,
   ...state,
