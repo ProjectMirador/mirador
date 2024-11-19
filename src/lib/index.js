3c67675f1a519def0cdbfd2e8806246252adccc0
Chris Beer <cabeer@stanford.edu>
2024-11-19 09:45:04 -0800

    Add index.js files to src directories

---
+++ src/lib/index.js
@@ -0,0 +1,23 @@
+import getBestThumbnail, { ThumbnailFactory } from './ThumbnailFactory';
+
+export { getBestThumbnail, ThumbnailFactory };
+
+export * as AnnotationFactory from './AnnotationFactory';
+export * as AnnotationItem from './AnnotationItem';
+export * as AnnotationList from './AnnotationList';
+export * as AnnotationPage from './AnnotationPage';
+export * as AnnotationResource from './AnnotationResource';
+export * as CanvasAnnotationDisplay from './CanvasAnnotationDisplay';
+export * as CanvasGroupings from './CanvasGroupings';
+export * as CanvasWorld from './CanvasWorld';
+export * as CompanionWindowRegistry from './CompanionWindowRegistry';
+export * as MiradorCanvas from './MiradorCanvas';
+export * as MiradorManifest from './MiradorManifest';
+export * as MiradorViewer from './MiradorViewer';
+export * as MosaicLayout from './MosaicLayout';
+export * as OpenSeadragonCanvasOverlay from './OpenSeadragonCanvasOverlay';
+
+export * as TruncatedHit from './TruncatedHit';
+export * as asArray from './asArray';
+export * as htmlRules from './htmlRules';
+export * from './readImageMetadata';
