da8fcaab9ed5dfbdefc877eb9bbc217c09d6cdbf
Chris Beer <cabeer@stanford.edu>
2024-11-20 13:49:36 -0800

    Convert NewWindow to a function.

--- src/components/NewWindow.js
+++ src/components/NewWindow.js
@@ -1,66 +1,30 @@
-import { Component } from 'react';
+import { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-/**
- * Opens a new window for click
- */
-export class NewWindow extends Component {
-  /** */
-  constructor(props) {
-    super(props);
+/** */
+export function NewWindow({
+  depWindow = undefined, features = undefined, name = undefined, onClose, url,
+}) {
+  const released = useRef(false);
 
-    this.released = undefined;
-    this.window = null;
-    this.checkIfWindowClosed = null;
-  }
+  useEffect(() => {
+    const newWindow = (depWindow || window).open(url, name, features);
 
-  /** */
-  componentDidMount() {
-    this.openWindow();
-  }
-
-  /**
-   * Close the opened window we unmount
-   */
-  componentWillUnmount() {
-    if (this.window) {
-      this.window.close();
-    }
-  }
-
-  /** @private */
-  onClose() {
-    const { onClose, url } = this.props;
-
-    if (this.released) return;
-
-    this.released = true;
-
-    clearInterval(this.checkIfWindowClosed);
-
-    onClose(url);
-  }
-
-  /** */
-  openWindow() {
-    const {
-      depWindow, features, name, url,
-    } = this.props;
-
-    this.window = (depWindow || window).open(url, name, features);
-    this.released = false;
-
-    this.checkIfWindowClosed = setInterval(() => {
-      if (!this.window || this.window.closed) {
-        this.onClose();
+    const checkIfWindowClosed = setInterval(() => {
+      if (!released.current && (!newWindow || newWindow.closed)) {
+        released.current = true;
+        clearInterval(checkIfWindowClosed);
+        onClose(url);
       }
     }, 250);
-  }
 
-  /** */
-  render() {
-    return null;
-  }
+    return () => {
+      clearInterval(checkIfWindowClosed);
+      newWindow.close();
+    };
+  });
+
+  return null;
 }
 
 NewWindow.propTypes = {
@@ -69,10 +33,4 @@ NewWindow.propTypes = {
   name: PropTypes.string,
   onClose: PropTypes.func.isRequired,
   url: PropTypes.string.isRequired,
-};
-
-NewWindow.defaultProps = {
-  depWindow: undefined,
-  features: undefined,
-  name: undefined,
 };
