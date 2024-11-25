9603e9bd345b6973218206eaea0928cf64dd6b3b
Fabian Stoehr <stoehr@ub.uni-leipzig.de>
2024-11-25 14:29:19 +0100

    refactor: use designers styling preference

--- src/components/WindowViewSettings.js
+++ src/components/WindowViewSettings.js
@@ -16,8 +16,17 @@ const ViewOption = styled(MenuItem, { name: 'WindowViewSettings', slot: 'option'
     ...(selected && {
       borderBottomColor: theme.palette.secondary.main,
     }),
-    backgroundColor: 'transparent !important',
+    '&.Mui-selected': {
+      backgroundColor: 'transparent !important',
+    },
+    '&.Mui-selected.Mui-focusVisible': {
+      backgroundColor: `${(theme.vars || theme).palette.action.focus} !important`,
+    },
+    '&:focused': {
+      backgroundColor: `${(theme.vars || theme).palette.action.focus} !important`,
+    },
     color: selected ? theme.palette.secondary.main : undefined,
+    display: 'inline-block',
   },
 }));
 
