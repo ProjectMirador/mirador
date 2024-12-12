c03da18a4d4c350b2cb143d3b63f744eb55803a5
Chris Beer <cabeer@stanford.edu>
2024-12-12 14:47:40 -0800

    Don't let manifesto mutate the manifest json.

--- src/state/selectors/manifests.js
+++ src/state/selectors/manifests.js
@@ -10,7 +10,7 @@ import { getConfig } from './config';
 /** */
 function createManifestoInstance(json, locale) {
   if (!json) return undefined;
-  const manifestoObject = Utils.parseManifest(json, locale ? { locale } : undefined);
+  const manifestoObject = Utils.parseManifest(JSON.parse(JSON.stringify(json)), locale ? { locale } : undefined);
   // Local patching of Manifesto so that when its a Collection, it behaves similarly
   if (typeof manifestoObject.getSequences != 'function') {
     manifestoObject.getSequences = () => [];
