a9c8da7a6d25f057d1bc910ee9af92a427961344
Lutz Helm <helm@ub.uni-leipzig.de>
2024-12-13 15:24:38 +0100

    Expect zero value for opacity and index in OpenSeadragonTileSource

--- src/components/OpenSeadragonTileSource.js
+++ src/components/OpenSeadragonTileSource.js
@@ -11,7 +11,7 @@ export default function OpenSeadragonTileSource({
   const tiledImage = useRef(undefined);
 
   useEffect(() => {
-    if (!opacity) return;
+    if (opacity == null) return;
 
     tiledImage.current?.setOpacity(opacity);
   }, [opacity]);
@@ -23,7 +23,7 @@ export default function OpenSeadragonTileSource({
   }, [fitBounds]);
 
   useEffect(() => {
-    if (!tiledImage.current || !viewer?.current || !index) return;
+    if (!tiledImage.current || !viewer?.current || index == null) return;
 
     viewer.current.world.setItemIndex(tiledImage.current, index);
   }, [index, viewer]);
