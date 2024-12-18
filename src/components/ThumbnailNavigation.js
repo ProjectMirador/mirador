d8e9bdeba085b0aed97cd4ca0116c08b06a0f7da
Chris Beer <cabeer@stanford.edu>
2024-12-18 10:43:03 -0800

    Only render the thumbnails if there are canvas groupings in the first place.

--- src/components/ThumbnailNavigation.js
+++ src/components/ThumbnailNavigation.js
@@ -165,6 +165,7 @@ export function ThumbnailNavigation({
       role="grid"
     >
       <div role="row" style={{ height: '100%', width: '100%' }}>
+        { canvasGroupings.length > 0 && (
         <AutoSizer
           defaultHeight={100}
           defaultWidth={400}
@@ -184,6 +185,7 @@ export function ThumbnailNavigation({
             </List>
           )}
         </AutoSizer>
+        )}
       </div>
     </Paper>
   );
