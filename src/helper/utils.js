9dbd54a89ce613ee7d4f7ce6f0083a9d31add320
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-02-08 20:21:37 +0100

    Fix linting issue

--- src/helper/utils.js
+++ src/helper/utils.js
@@ -1,8 +1,11 @@
 /**
  * Filter annotation with a query string. Search in ID and value
  * */
-export const filterAnnotation = (annotations, query) => annotations.filter((annotation) => {
-  // eslint-disable-next-line max-len
-  const queryLowered = query.toLowerCase();
-  return annotation.id.toLowerCase().includes(queryLowered) || (annotation?.content && annotation?.content.toLowerCase().includes(queryLowered));
-});
+export const filterAnnotation = (annotations, query) => {
+  return annotations.filter((annotation) => {
+    // eslint-disable-next-line max-len
+    const queryLowered = query.toLowerCase();
+    return annotation.id.toLowerCase().includes(queryLowered) ||
+        (annotation?.content && annotation?.content.toLowerCase().includes(queryLowered));
+  });
+};
