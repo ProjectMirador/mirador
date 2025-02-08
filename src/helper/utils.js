4663edb099af24727d0ede72edb9989b7209c5c3
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-02-08 20:50:06 +0100

    Use new data in annotation filter

--- src/helper/utils.js
+++ src/helper/utils.js
@@ -4,6 +4,9 @@
 export const filterAnnotation = (annotations, query) => annotations.filter((annotation) => {
   // eslint-disable-next-line max-len
   const queryLowered = query.toLowerCase();
+  console.log('Annotation', annotation);
   return annotation.id.toLowerCase().includes(queryLowered)
-        || (annotation?.content && annotation?.content.toLowerCase().includes(queryLowered));
+        || (annotation?.content && annotation?.content.toLowerCase().includes(queryLowered))
+        || (annotation?.creator && annotation?.creator.toLowerCase().includes(queryLowered))
+        || (annotation?.lastEditor && annotation?.lastEditor.toLowerCase().includes(queryLowered));
 });
