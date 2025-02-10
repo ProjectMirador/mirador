63dc207337ba7e42f33c6d1a79d1731a96f1590b
Marcel Tronco <marcel.tronco@posteo.de>
2025-02-10 18:08:11 +0100

    add translate hook to plugins

--- src/components/PluginHook.js
+++ src/components/PluginHook.js
@@ -1,16 +1,19 @@
 import { forwardRef, isValidElement, cloneElement } from 'react';
+import { useTranslation } from 'react-i18next';
 
 /** Renders plugins */
 export const PluginHook = forwardRef((props, ref) => {
   const { PluginComponents } = props; // eslint-disable-line react/prop-types
   const { classes, ...otherProps } = props; // eslint-disable-line react/prop-types
+  const {t} = useTranslation()
   return PluginComponents ? (
     PluginComponents.map((PluginComponent, index) => ( // eslint-disable-line react/prop-types
       isValidElement(PluginComponent)
-        ? cloneElement(PluginComponent, { ...otherProps, ref })
+        ? cloneElement(PluginComponent, {t,  ...otherProps, ref })
         : (
           <PluginComponent
             ref={ref}
+            t={t}
             {...otherProps}
             key={index} // eslint-disable-line react/no-array-index-key
           />
