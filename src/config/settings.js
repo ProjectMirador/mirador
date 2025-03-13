5b0af56293632cb64fd10aa920e8a57c1d7ae24f
Justin Coyne <jcoyne@justincoyne.com>
2025-03-13 11:53:29 -0500

    Fix typo in settings

--- src/config/settings.js
+++ src/config/settings.js
@@ -526,7 +526,7 @@ export default {
     id: uuid(),
     isWorkspaceAddVisible: false, // Catalog/Workspace add window feature visible by default
     showZoomControls: true, // Configure if zoom controls should be displayed by default
-    type: 'mosaic', // Which workspace type to load by default. Other possible values are "elastic". If "mosaic" or "elastic" are not selected no worksapce type will be used.
+    type: 'mosaic', // Which workspace type to load by default. Other possible values are "elastic". If "mosaic" or "elastic" are not selected no workspace type will be used.
     viewportPosition: { // center coordinates for the elastic mode workspace
       x: 0,
       y: 0,
