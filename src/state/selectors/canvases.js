db553742d39b989492362ab124619dd4c323dc60
Lutz Helm <helm@ub.uni-leipzig.de>
2025-05-09 09:15:26 +0200

    Fix check for IIIF image services in getVisibleCanvasNonTiledResources

--- src/state/selectors/canvases.js
+++ src/state/selectors/canvases.js
@@ -216,7 +216,7 @@ export const getVisibleCanvasNonTiledResources = createSelector(
   ],
   (canvases, getMiradorCanvas) => flatten(canvases
     .map(canvas => getMiradorCanvas(canvas).imageResources))
-    .filter(resource => getIiifResourceImageService(resource).length < 1),
+    .filter(resource => !getIiifResourceImageService(resource)),
 );
 
 /**
