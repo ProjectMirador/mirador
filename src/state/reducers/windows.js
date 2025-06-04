a286772d77ac2445abb25dad9904eee755297f41
fstoe <stoehr.fabian90@gmail.com>
2025-06-04 16:23:38 +0200

    chore: add missing trailing comma

--- src/state/reducers/windows.js
+++ src/state/reducers/windows.js
@@ -166,7 +166,7 @@ export const windowsReducer = (state = {}, action) => {
         [action.windowId]: {
           ...state[action.windowId],
           collectionDialogOn: false,
-          collectionPath: action.collectionPath
+          collectionPath: action.collectionPath,
         },
       };
     default:
