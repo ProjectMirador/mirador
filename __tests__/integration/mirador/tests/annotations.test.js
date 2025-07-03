758d65f8fabc8a2dfa9f70c85fe761d534d46575
Lutz Helm <helm@ub.uni-leipzig.de>
2025-07-03 11:04:16 +0200

    Update number of annotations for resource that is used as fixture again

--- __tests__/integration/mirador/tests/annotations.test.js
+++ __tests__/integration/mirador/tests/annotations.test.js
@@ -42,12 +42,12 @@ describe('Annotations in Mirador', () => {
 
     expect(await screen.findByRole('heading', { name: 'Annotations' })).toBeInTheDocument();
 
-    expect(await screen.findByText('Showing 6 annotations')).toBeInTheDocument();
+    expect(await screen.findByText('Showing 12 annotations')).toBeInTheDocument();
 
     const annotationPanel = await screen.findByRole('complementary', { name: /annotations/i });
     expect(annotationPanel).toBeInTheDocument();
 
     const listItems = await within(annotationPanel).findAllByRole('menuitem');
-    expect(listItems).toHaveLength(6);
+    expect(listItems).toHaveLength(12);
   });
 });
