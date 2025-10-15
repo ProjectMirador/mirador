2d2960acdf16388066282d637ec80d7c17672ab1
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-10-15 16:27:23 +0200

    Fix build error

--- vite.demo.config.js
+++ vite.demo.config.js
@@ -1,15 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 
 export default defineConfig({
   base: process.env.BASE_PATH || `/${process.env.npm_package_name}/`,
-  root: fileURLToPath(new URL('./__tests__/integration/mirador/', import.meta.url)),
   build: {
     emptyOutDir: true,
     outDir: fileURLToPath(new URL('./dist', import.meta.url)),
     sourcemap: true,
   },
   plugins: [react()],
+  root: fileURLToPath(new URL('./__tests__/integration/mirador/', import.meta.url)),
 });
