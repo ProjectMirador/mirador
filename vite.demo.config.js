8d14777546acf6a893af0bfba1eac402cdc6b6f2
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-10-15 16:23:03 +0200

    WIP CI deploy demo

--- vite.demo.config.js
+++ vite.demo.config.js
@@ -1,16 +1,15 @@
-// vite.config.js (or .ts)
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 
 export default defineConfig({
-
+  base: process.env.BASE_PATH || `/${process.env.npm_package_name}/`,
+  root: fileURLToPath(new URL('./__tests__/integration/mirador/', import.meta.url)),
   build: {
     emptyOutDir: true,
     outDir: fileURLToPath(new URL('./dist', import.meta.url)),
     sourcemap: true,
   },
   plugins: [react()],
-  root: fileURLToPath(new URL('./__tests__/integration/mirador/', import.meta.url)),
-
 });
