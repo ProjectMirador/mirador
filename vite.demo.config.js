8edda10ba6ee75f600fcd226ae19d68116e1ef1c
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-10-21 14:07:50 +0200

    Auto deploy demo on CI

---
+++ vite.demo.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+
+export default defineConfig({
+  base: process.env.BASE_PATH || `/${process.env.npm_package_name}/`,
+  build: {
+    emptyOutDir: true,
+    outDir: fileURLToPath(new URL('./dist', import.meta.url)),
+    sourcemap: true,
+  },
+  plugins: [react()],
+  root: fileURLToPath(new URL('./__tests__/integration/mirador/', import.meta.url)),
+});
