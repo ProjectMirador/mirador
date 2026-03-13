e58cae24d7ef606c748d0f07394dab014b240358
Justin Coyne <jcoyne@justincoyne.com>
2026-03-13 13:18:38 -0500

    Destructure props for StateDependentComponent

--- __tests__/integration/mirador/components/StateDependentComponent.jsx
+++ __tests__/integration/mirador/components/StateDependentComponent.jsx
@@ -1,7 +1,7 @@
-const StateDependentComponent = (props) => (
-    <button id="plugin-button" onClick={props.incSomeNumber} style={{ width: 64 }}>
-      { "Plugin:" + props.manifestId }
-    </button>
-  );
+const StateDependentComponent = ({ incSomeNumber, manifestId }) => (
+  <button id="plugin-button" onClick={incSomeNumber} style={{ width: 64 }}>
+      { "Plugin:" + manifestId }
+  </button>
+);
 
-  export default StateDependentComponent;
+export default StateDependentComponent;
