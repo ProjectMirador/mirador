2d71258da0f8b512fccc44a5db58b2f6dfbc4ab1
Chris Beer <cabeer@stanford.edu>
2026-08-24 08:12:54 -0700

    Extract a reusable method for converting legacy action parameters to the new slice actions.

--- src/state/actions/layers.js
+++ src/state/actions/layers.js
@@ -1,9 +1,23 @@
 import { updateLayers as updateLayersSliceAction } from '../slices';
 
-export function updateLayers(windowIdOrPayload, canvasId, payload) {
-  if (typeof windowIdOrPayload === 'string') {
-    return updateLayersSliceAction({ windowId: windowIdOrPayload, canvasId, ...payload });
-  }
+function convertToSliceAction(forwardedAction, legacyParamNames) {
+  return (...args) => {
+    if (args.length === 1 && typeof args[0] === 'object') {
+      return forwardedAction(args[0]);
+    }
 
-  return updateLayersSliceAction(windowIdOrPayload);
+    let payload = {};
+
+    legacyParamNames.forEach((paramName, index) => {
+      if (paramName === 'payload') {
+        payload = { ...payload, ...args[index] };
+      } else {
+        payload[paramName] = args[index];
+      }
+    });
+
+    return forwardedAction(payload);
+  }
 }
+
+export const updateLayers = convertToSliceAction(updateLayersSliceAction, ['windowId', 'canvasId', 'payload']);
